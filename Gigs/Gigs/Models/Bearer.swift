86fc7f500aca93ae1eb90a68bb2114e183d6e726
Christopher Aronson <christopheraronson@icloud.com>
2019-05-09 13:35:03 -0700

    Created Gig, Bearer, and User object files

---
+++ Gigs/Gigs/Models/Bearer.swift
@@ -0,0 +1,13 @@
+//
+//  Bearer.swift
+//  Gigs
+//
+//  Created by Christopher Aronson on 5/9/19.
+//  Copyright © 2019 Christopher Aronson. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer {
+    let token: String
+}
