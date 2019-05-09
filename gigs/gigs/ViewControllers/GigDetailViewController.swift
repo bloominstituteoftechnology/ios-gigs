abd4b5efae64882c9c70a041cdc99c7e4fcfa22b
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-09 14:30:11 -0700

    GigDetailView

---
+++ gigs/gigs/ViewControllers/GigDetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  GigDetailViewController.swift
+//  gigs
+//
+//  Created by Hector Steven on 5/9/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class GigDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
