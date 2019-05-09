2eacca25f2c13041bc1aa0d393bd145a8500226b
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-09 15:49:32 -0700

    conform to decodable

--- gigs/gigs/Controller/GigController.swift
+++ gigs/gigs/Controller/GigController.swift
@@ -47,12 +47,55 @@ class GigController {
 	
 	func signIn(user: User, completion: @escaping (Error?) -> ()) {
 		let url = baseURL.appendingPathComponent("users/login")
-		
 		var request = URLRequest(url: url)
+		request.httpMethod = HTTPMethod.post.rawValue
+		request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+		
+		let encoder = JSONEncoder()
+		do {
+			let jsonData = try encoder.encode(user)
+			request.httpBody = jsonData
+		} catch {
+			print("error encoding user object\(error)")
+			completion(error)
+			return
+		}
+		
+		URLSession.shared.dataTask(with: request) {
+			data, response, error in
+			if let response = response as? HTTPURLResponse,
+				response.statusCode != 200 {
+				completion(NSError(domain: "", code: response.statusCode, userInfo: nil))
+				return
+			}
+			
+			if let error = error {
+				completion(NSError())
+				return
+			}
+			
+			guard let data = data else {
+				completion(NSError())
+				return
+			}
+			
+			let decoder = JSONDecoder()
+			do {
+				self.bearer = try decoder.decode(Bearer.self, from: data)
+			} catch {
+				print("error decoding bearrer object: \(error)")
+				completion(error)
+				return
+			}
+			
+			
+			
+		}
+		
 	}
 	
 
 	private(set) var gigs: [Gig] = []
-	var bearer: String?
+	var bearer: Bearer?
 	private let baseURL = URL(string: "https://lambdagigs.vapor.cloud/api")!
 }
