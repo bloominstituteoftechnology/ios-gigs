c7bf3a9a2fe5df59d63a2b40094d552f5b14df1e
Lisa Sampson <lisasampson0918@gmail.com>
2019-05-09 19:58:11 -0400

    Set up detail view

--- Gigs/Gigs/View Controllers/GigDetailViewController.swift
+++ Gigs/Gigs/View Controllers/GigDetailViewController.swift
@@ -10,28 +10,59 @@ import UIKit
 
 class GigDetailViewController: UIViewController {
     
+    // MARK: - Properties and Outlets
+    
     @IBOutlet weak var gigTextField: UITextField!
     @IBOutlet weak var dueDatePicker: UIDatePicker!
     @IBOutlet weak var descTextView: UITextView!
     
+    var gigController: GigController!
+    var gig: Gig?
+    
+    // MARK: - View Loading Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        updateViews()
+    }
+    
+    func updateViews() {
+        if let gig = gig {
+            
+            self.title = gig.title
+            gigTextField.text = gig.title
+            dueDatePicker.date = gig.dueDate
+            descTextView.text = gig.description
+            
+            self.navigationItem.rightBarButtonItem?.isEnabled = false
+            descTextView.isEditable = false
+        } else {
+            self.title = "New Gig"
+        }
     }
     
     @IBAction func saveButtonTapped(_ sender: Any) {
         
+        guard let gig = gigTextField.text,
+            !gig.isEmpty,
+            let description = descTextView.text,
+            !description.isEmpty else { return }
+        
+        let dueDate = dueDatePicker.date
+        let newGig = Gig(title: gig, description: description, dueDate: dueDate)
+        
+        gigController.createGig(with: newGig) { (error) in
+            if let error = error {
+                NSLog("Error saving new gig: \(error)")
+                return
+            }
+            
+            DispatchQueue.main.async {
+                self.gig = newGig
+                self.updateViews()
+                self.navigationController?.popViewController(animated: true)
+            }
+        }
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
