6637155692973a40e35c657c7bf2e2aad62de813
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-09 14:48:34 -0700

    gigController

---
+++ gigs/gigs/Controller/GigController.swift
@@ -0,0 +1,13 @@
+//
+//  GigController.swift
+//  gigs
+//
+//  Created by Hector Steven on 5/9/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import Foundation
+
+class GigController {
+	
+}
