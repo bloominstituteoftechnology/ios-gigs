876fe116f0f02a741ff1aa3857079daa70ca1599
Lisa Sampson <lisasampson0918@gmail.com>
2019-05-09 18:40:08 -0400

    Create model files

---
+++ Gigs/Gigs/Models/Gig.swift
@@ -0,0 +1,15 @@
+//
+//  Gig.swift
+//  Gigs
+//
+//  Created by Lisa Sampson on 5/9/19.
+//  Copyright © 2019 Lisa M Sampson. All rights reserved.
+//
+
+import Foundation
+
+struct Gig: Codable {
+    let title: String
+    let description: String
+    let dueDate: Date
+}
