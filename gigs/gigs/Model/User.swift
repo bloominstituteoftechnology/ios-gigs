43e8ee9f3d649f283e2e4589af0090045491af90
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-09 14:44:25 -0700

    models

---
+++ gigs/gigs/Model/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  gigs
+//
+//  Created by Hector Steven on 5/9/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import Foundation
+struct User {
+	let username: String
+	let password: String
+}
