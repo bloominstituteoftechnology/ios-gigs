487f4b69c2e776faa89704d1bfb7b08acdbca47b
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-09 16:27:48 -0700

    cellforrowat numberofrowsinsection

--- gigs/gigs/ViewControllers/GigsTableViewController.swift
+++ gigs/gigs/ViewControllers/GigsTableViewController.swift
@@ -14,7 +14,22 @@ class GigsTableViewController: UITableViewController {
         super.viewDidLoad()
 
     }
+	
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return gigController.gigs.count
+	}
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "GigCell", for: indexPath)
+		let gig = gigController.gigs[indexPath.row]
+		
+		cell.textLabel?.text = gig.title
+		cell.detailTextLabel?.text = gig.description
+		
+		return cell
+	}
+	
 
-
-
+	let gigController = GigController()
+	
 }
