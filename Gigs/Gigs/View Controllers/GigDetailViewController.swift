aded45f85c9da450db60b3690694905956212c01
Jeffrey Carpenter <spaz926@me.com>
2019-05-09 17:46:45 -0500

    Fixed an issue where the keyboard would cover the textView

--- Gigs/Gigs/View Controllers/GigDetailViewController.swift
+++ Gigs/Gigs/View Controllers/GigDetailViewController.swift
@@ -20,7 +20,37 @@ class GigDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Listen for keyboard events
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        // Listen for tap events
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
+        
         updateViews()
+    }
+    
+    @objc private func keyboardWillShow(notification: NSNotification) {
+        
+        guard gigDescriptionTextView.isFirstResponder else { return }
+        
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+    
+    @objc private func keyboardWillHide(notification: NSNotification) {
+        
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
+    
+    @objc private func dismissKeyboard() {
+        view.endEditing(true)
     }
     
     @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
@@ -66,6 +96,7 @@ class GigDetailViewController: UIViewController {
             gigDescriptionTextView.isEditable = false
         } else {
             self.title = "New Gig"
+            gigDescriptionTextView.text = "Enter description"
         }
     }
 }
