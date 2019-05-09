6405d34d538c7497a8022d0b2a9f7c34221ab958
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-09 14:11:48 -0700

    loginVC

---
+++ gigs/gigs/ViewControllers/GigsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  GigsTableViewController.swift
+//  gigs
+//
+//  Created by Hector Steven on 5/9/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class GigsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+
+}
---
+++ gigs/gigs/ViewControllers/LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginViewController.swift
+//  gigs
+//
+//  Created by Hector Steven on 5/9/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+	
+	@IBAction func singInUpButton(_ sender: UIButton) {
+	
+	
+	}
+	
+	@IBAction func segmentedControlValueChanged(_ sender: UISegmentedControl) {
+	
+	}
+	
+	@IBOutlet var signInUpButtonOutlet: UIButton!
+	@IBOutlet var usernameTextField: UITextField!
+	@IBOutlet var passwordTextField: UITextField!
+	@IBOutlet var segmentedControl: UISegmentedControl!
+	
+}
