1750519222807ab85d47256902b9f59a807a74e1
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-09 17:20:56 -0700

    setWithSegmentedControl

--- gigs/gigs/ViewControllers/LoginViewController.swift
+++ gigs/gigs/ViewControllers/LoginViewController.swift
@@ -17,21 +17,52 @@ class LoginViewController: UIViewController {
 		signInUpButtonOutlet.layer.cornerRadius = 8.0
     }
 	
-	@IBAction func singInUpButton(_ sender: UIButton) {
-		
-		
-		
-	}
 	
-	@IBAction func segmentedControlValueChanged(_ sender: UISegmentedControl) {
-		if sender.selectedSegmentIndex == 0 {
+	func setWithSegmentedControl(_ index: Int) {
+		if index == 0 {
 			loginType = .SignUp
 			signInUpButtonOutlet.setTitle("Sign Up", for: .normal)
 		} else {
 			loginType = .SignIn
-		signInUpButtonOutlet.setTitle("Sign In", for: .normal)
+			signInUpButtonOutlet.setTitle("Sign In", for: .normal)
 		}
 	}
+	
+	@IBAction func segmentedControlValueChanged(_ sender: UISegmentedControl) {
+		setWithSegmentedControl(sender.selectedSegmentIndex)
+	}
+
+	@IBAction func singInUpButton(_ sender: UIButton) {
+		guard let username = usernameTextField.text,
+			let password = passwordTextField.text else { return }
+		
+		let user = User(username: username, password: password)
+		
+		if loginType == .SignUp {
+			gigController?.signUp(with: user, completion: {
+				error in
+				if let error = error {
+					print("error with sign up: \(error)")
+				} else {
+					DispatchQueue.main.async {
+						self.pleaseLogInAlert()
+					}
+				}
+			})
+		} else {
+			
+		}
+	}
+	
+	
+	func pleaseLogInAlert(){
+		let alertController = UIAlertController(title: "Please Log In", message: nil, preferredStyle: .alert)
+		let alertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+		alertController.addAction(alertAction)
+		
+		
+	}
+	
 	
 	@IBOutlet var signInUpButtonOutlet: UIButton!
 	@IBOutlet var usernameTextField: UITextField!
