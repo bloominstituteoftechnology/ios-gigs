88c59e9c278403d27211e9dcff48cb72bd3dd5d8
Michael Redig <mredig@gmail.com>
2019-05-09 23:58:29 -0500

    mostly finished the login controller

--- Gigs/Gigs/View Controllers/GigsTableViewController.swift
+++ Gigs/Gigs/View Controllers/GigsTableViewController.swift
@@ -10,20 +10,58 @@ import UIKit
 
 class GigsTableViewController: UITableViewController {
 
+	let gigController = GigController()
+	let dateFormatter = DateFormatter()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+		dateFormatter.dateStyle = .short
+		dateFormatter.timeStyle = .short
     }
 
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
 
+		if gigController.bearer == nil {
+			performSegue(withIdentifier: "ShowLoginVC", sender: self)
+		} else {
+			loadData()
+		}
+	}
+
+	func loadData() {
+		gigController.getAllGigs { [weak self] (error) in
+			if let error = error {
+				print("Error happened: \(error)")
+			}
+			DispatchQueue.main.async {
+				self?.tableView.reloadData()
+			}
+		}
+	}
+
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+		if segue.identifier == "ShowLoginVC" {
+			if let dest = segue.destination as? LoginViewController {
+				dest.gigController = gigController
+			}
+		}
+	}
 }
 // MARK: - Table view data source
 extension GigsTableViewController {
 	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return 0
+		return gigController.gigs.count
 	}
 
 	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCell(withIdentifier: "GigCell", for: indexPath)
+
+		let gig = gigController.gigs[indexPath.row]
+
+		cell.textLabel?.text = gig.title
+		cell.detailTextLabel?.text = dateFormatter.string(from: gig.dueDate)
+
 		return cell
 	}
 }
