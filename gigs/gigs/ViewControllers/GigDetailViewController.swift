5c8dad1250fe2844bbdf94b91eeef396c5862b03
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-09 19:02:27 -0700

    Save will create gig

--- gigs/gigs/ViewControllers/GigDetailViewController.swift
+++ gigs/gigs/ViewControllers/GigDetailViewController.swift
@@ -13,11 +13,24 @@ class GigDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 		navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(save))
-        
+        descriptionTextView.text = ""
     }
 	
 	@objc func save() {
-	
+		guard let description = descriptionTextView.text,
+			let title = jobTitleTextField.text else { return }
+		
+		let gig = Gig(title: title, description: description, dueDate: datePicker.date)
+		
+		gigController.creatGig(gig: gig) { error in
+			if let error = error {
+				print("error creating gig: \(error)")
+			} else {
+				DispatchQueue.main.async {
+					self.dismiss(animated: true, completion: nil)
+				}
+			}
+		}
 	
 	}
 
