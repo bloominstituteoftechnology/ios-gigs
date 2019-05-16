b41901936bc67650f6a1f4f7f881e55decab7b15
Kobe McKee <kobe.mckee@gmail.com>
2019-05-16 15:52:00 -0600

    Fleshed out LoginViewController

--- Gigs/Gigs/ViewController/LoginViewController.swift
+++ Gigs/Gigs/ViewController/LoginViewController.swift
@@ -9,45 +9,81 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    var gigController: GigController!
+    
+    var loginType: LoginType = .signUp
+    
+    
+    enum LoginType {
+        case logIn
+        case signUp
+    }
+    
 
     @IBOutlet weak var entryMethodSegmentedControl: UISegmentedControl!
-    
-    
     @IBOutlet weak var nameTextField: UITextField!
-    
-    
     @IBOutlet weak var passwordTextField: UITextField!
-    
     @IBOutlet weak var enterButton: UIButton!
     
     
-    
     @IBAction func entryMethodChanged(_ sender: UISegmentedControl) {
+        if entryMethodSegmentedControl.selectedSegmentIndex == 0 {
+            loginType = .signUp
+            enterButton.setTitle("Sign Up", for: .normal)
+            navigationItem.title = "Sign Up"
+        } else {
+            loginType = .logIn
+            enterButton.setTitle("Log In", for: .normal)
+            navigationItem.title = "Log In"
+        }
     }
     
-    
-    
+
     @IBAction func enterButtonPressed(_ sender: UIButton) {
+        
+        guard let username = nameTextField.text,
+            let password = passwordTextField.text else { return }
+        
+        
+        if loginType == .signUp {
+            gigController.signUp(with: username, password: password) { (error) in
+                if let error = error {
+                    NSLog("Error signing up \(error)")
+                } else {
+                    DispatchQueue.main.async {
+                        let alertController = UIAlertController(title: "Sign up Successful", message: "Now please log in.", preferredStyle: .alert)
+                        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                        alertController.addAction(alertAction)
+                        self.present(alertController, animated: true, completion: {
+                            self.loginType = .logIn
+                            self.entryMethodSegmentedControl.selectedSegmentIndex = 1
+                            self.enterButton.setTitle("Sign In", for: .normal)
+                        })
+                    }
+                }
+            }
+        } else {
+            gigController.logIn(with: username, password: password) { (error) in
+                
+                if let error = error {
+                    NSLog("Error logging in: \(error)")
+                } else {
+                    DispatchQueue.main.async {
+                        self.dismiss(animated: true, completion: nil)
+                    }
+                }
+            }
+            
+        }
+        
+        
+        
     }
-    
-    
-    
-    
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
