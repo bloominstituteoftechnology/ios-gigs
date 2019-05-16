b7a5730dee6c9c0971ad3ef4f35181dbc429909f
Hayden Hastings <haydenhastings32@gmail.com>
2019-05-16 14:44:27 -0600

    Added files with properties Bearer, Gigs, User and a modelController GigController

---
+++ Gigs/Gigs/Model Controller/GigController.swift
@@ -0,0 +1,13 @@
+//
+//  GigController.swift
+//  Gigs
+//
+//  Created by Hayden Hastings on 5/16/19.
+//  Copyright © 2019 Hayden Hastings. All rights reserved.
+//
+
+import Foundation
+
+class GigController {
+    
+}
