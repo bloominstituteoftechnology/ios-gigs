3662bdbe03f931895563f1db9f0a38e2b7eaed28
jayfenam <jonathanferrer95@gmail.com>
2019-05-16 16:13:41 -0400

    created the models

---
+++ Gigs/Gigs/Model/Bearer.swift
@@ -0,0 +1,13 @@
+//
+//  Bearer.swift
+//  Gigs
+//
+//  Created by Jonathan Ferrer on 5/16/19.
+//  Copyright © 2019 Jonathan Ferrer. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer: Codable {
+    let token: String
+}
