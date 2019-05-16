7268c717bec39f883b003c336b00480800029591
Taylor Lyles <hawks93187@gmail.com>
2019-05-16 13:35:52 -0700

    Models: Added Gig, Bearer, and User model

---
+++ gigs/gigs/Models/Bearer.swift
@@ -0,0 +1,13 @@
+//
+//  Bearer.swift
+//  gigs
+//
+//  Created by Taylor Lyles on 5/16/19.
+//  Copyright © 2019 Taylor Lyles. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer: Codable {
+	let token: String
+}
---
+++ gigs/gigs/ViewController/GigController.swift
@@ -0,0 +1,14 @@
+//
+//  GigController.swift
+//  gigs
+//
+//  Created by Taylor Lyles on 5/16/19.
+//  Copyright © 2019 Taylor Lyles. All rights reserved.
+//
+
+import Foundation
+
+
+class GigController: <#super class#> {
+	
+}
