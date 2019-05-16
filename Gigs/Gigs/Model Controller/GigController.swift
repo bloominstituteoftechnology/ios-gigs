67d42d6ce891fdb591877e7eb2112447432d2eea
Hayden Hastings <haydenhastings32@gmail.com>
2019-05-16 15:36:02 -0600

    Added Codable to User file. Added signUp and logIn Functions with  respected code in GigController.

--- Gigs/Gigs/Model Controller/GigController.swift
+++ Gigs/Gigs/Model Controller/GigController.swift
@@ -10,4 +10,109 @@ import Foundation
 
 class GigController {
     
+    // MARK: - Methods
+    
+    func signUp(with username: String, password: String, completion: @escaping (Error?) -> Void) {
+        
+        let requestURL = baseURL.appendingPathComponent("users/signup")
+        
+        var request = URLRequest(url: requestURL)
+        
+        request.httpMethod = HTTPMethod.post.rawValue
+        
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let user = User(username: username, password: password)
+        
+        do {
+            request.httpBody = try JSONEncoder().encode(user)
+        } catch {
+            NSLog("Error encoding User: \(error)")
+            completion(error)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { (_, response, error) in
+            
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                completion(NSError())
+                return
+            }
+            
+            if let error = error {
+                NSLog("Error signing up: \(error)")
+                completion(error)
+                return
+            }
+            }.resume()
+    }
+    
+    func logIn(with username: String, password: String, completion: @escaping (Error?) -> Void) {
+        
+        let requestURL = baseURL.appendingPathComponent("users/login")
+        
+        var request = URLRequest(url: requestURL)
+        
+        request.httpMethod = HTTPMethod.post.rawValue
+        
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let user = User(username: username, password: password)
+        
+        do {
+            request.httpBody = try JSONEncoder().encode(user)
+        } catch {
+            NSLog("Error encoding User: \(error)")
+            completion(error)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                completion(NSError())
+                return
+            }
+            
+            if let error = error {
+                NSLog("Error loggin in: \(error)")
+                completion(error)
+                return
+            }
+            
+            guard let data = data else {
+                NSLog("No data returned from data task")
+                completion(NSError())
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            
+            do {
+                let bearer = try decoder.decode(Bearer.self, from: data)
+                self.bearer = bearer
+                completion(nil)
+            } catch {
+                NSLog("Error decoding Bearer: \(error)")
+                completion(error)
+                return
+            }
+            }.resume()
+    }
+    
+    // MARK: - Properties
+    
+    let baseURL = URL(string: "https://lambdagigs.vapor.cloud/api")!
+    
+    var bearer: Bearer?
+    var gigs: [Gigs] = []
+    
+    enum HTTPMethod: String {
+        case get = "GET"
+        case put = "PUT"
+        case post = "POST"
+        case delete = "DELETE"
+    }
 }
