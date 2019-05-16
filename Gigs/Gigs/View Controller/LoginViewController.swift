b25fdcd2ef016b47eb408d3b214ffe858bbd08e7
Hayden Hastings <haydenhastings32@gmail.com>
2019-05-16 16:50:08 -0600

    Added Code to the IBActions in LoginViewController.

--- Gigs/Gigs/View Controller/LoginViewController.swift
+++ Gigs/Gigs/View Controller/LoginViewController.swift
@@ -27,11 +27,53 @@ class LoginViewController: UIViewController {
     }
     */
     @IBAction func signInTypeChanged(_ sender: Any) {
-        
+        if loginTypeSegmentedControl.selectedSegmentIndex == 0 {
+            signInType = .signUp
+            signInButton.setTitle("Sign Up", for: .normal)
+        } else {
+            signInType = .logIn
+            signInButton.setTitle("Log In", for: .normal)
+        }
     }
     
     @IBAction func authenticate(_ sender: Any) {
         
+        guard let username = usernameTextField.text,
+            let password = passwordTextField.text else { return }
+        switch signInType {
+            
+        case .signUp:
+            
+            gigController?.signUp(with: username, password: password, completion: { (error) in
+                
+                if let error = error {
+                    NSLog("Error signing up: \(error)")
+                } else {
+                    DispatchQueue.main.async {
+                        let alertController = UIAlertController(title: "Sign Up Successful", message: "Now please log in.", preferredStyle: .alert)
+                        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                        alertController.addAction(alertAction)
+                        self.present(alertController, animated: true, completion: {
+                            self.signInType = .logIn
+                            self.loginTypeSegmentedControl.selectedSegmentIndex = 1
+                            self.signInButton.setTitle("Sign In", for: .normal)
+                        })
+                    }
+                }
+            })
+            
+        case .logIn:
+            
+            gigController?.logIn(with: username, password: password, completion: { (error) in
+                if let error = error {
+                    NSLog("Error logging in: \(error)")
+                } else {
+                    DispatchQueue.main.async {
+                        self.dismiss(animated: true, completion: nil)
+                    }
+                }
+            })
+        }
     }
     
     @IBOutlet weak var usernameTextField: UITextField!
@@ -39,5 +81,11 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var loginTypeSegmentedControl: UISegmentedControl!
     @IBOutlet weak var signInButton: UIButton!
     
+    enum SignInType {
+        case signUp
+        case logIn
+    }
     
+    var signInType: SignInType = .signUp
+    var gigController: GigController!
 }
