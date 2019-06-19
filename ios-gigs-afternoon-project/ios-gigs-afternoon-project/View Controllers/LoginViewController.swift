fd80956bd9a1b8bc66c5226b9e6992bb3f6728a9
shillwil <alex.shillingford@icloud.com>
2019-06-19 13:27:12 -0600

    Part 1 completed: Storyboard created and wired up with accompanying view controllers

---
+++ ios-gigs-afternoon-project/ios-gigs-afternoon-project/View Controllers/LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginViewController.swift
+//  ios-gigs-afternoon-project
+//
+//  Created by Alex Shillingford on 6/19/19.
+//  Copyright © 2019 Alex Shillingford. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var signInButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func signInTypeChanged(_ sender: UISegmentedControl) {
+        
+    }
+    
+    @IBAction func signInButtonTapped(_ sender: UIButton) {
+        
+    }
+}
