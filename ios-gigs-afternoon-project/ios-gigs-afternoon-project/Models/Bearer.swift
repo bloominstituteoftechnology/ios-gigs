9b5407a572deecaaf28e2a61d16fb6be02fce656
shillwil <alex.shillingford@icloud.com>
2019-06-19 13:41:35 -0600

    Part 2 completed: Models created for Bearer and User structs w/ variables

---
+++ ios-gigs-afternoon-project/ios-gigs-afternoon-project/Models/Bearer.swift
@@ -0,0 +1,17 @@
+//
+//  Bearer.swift
+//  ios-gigs-afternoon-project
+//
+//  Created by Alex Shillingford on 6/19/19.
+//  Copyright © 2019 Alex Shillingford. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer: Codable {
+    enum KeyValue: String {
+        case identification = "id"
+        case token = "token"
+        case userID = "userId"
+    }
+}
---
+++ ios-gigs-afternoon-project/ios-gigs-afternoon-project/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  ios-gigs-afternoon-project
+//
+//  Created by Alex Shillingford on 6/19/19.
+//  Copyright © 2019 Alex Shillingford. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    var username: String
+    var password: String
+}
