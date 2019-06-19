9e40ffe255909b51bbe0d38eb3e4732b13759a00
Kat Milton <mltnkat@gmail.com>
2019-06-19 12:44:35 -0700

    MVP Upload for the day.

---
+++ ios-Gigs/Model/GigController.swift
@@ -0,0 +1,105 @@
+//
+//  GigController.swift
+//  ios-Gigs
+//
+//  Created by Kat Milton on 6/19/19.
+//  Copyright © 2019 Kat Milton. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum HTTPMethod: String {
+    case post = "POST"
+    case get = "GET"
+}
+
+class GigController {
+    
+    var bearer: Bearer?
+    private let baseURL = URL(string: "https://lambdagigs.vapor.cloud/api")!
+    
+    // Sign up
+    func signUp(with user: User, completion: @escaping (Error?) -> ()) {
+        let signUpURL = baseURL.appendingPathComponent("users/signup")
+        
+        var request = URLRequest(url: signUpURL)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-type")
+        
+        let jsonEncoder = JSONEncoder()
+        do {
+            let jsonData = try jsonEncoder.encode(user)
+            request.httpBody = jsonData
+        } catch {
+            print("Error encoding user object: \(error)")
+            completion(error)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { _, response, error in
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                completion(NSError(domain: "", code: response.statusCode, userInfo: nil))
+                return
+            }
+            
+            if let error = error {
+                completion(error)
+                return
+            }
+            
+            completion(nil)
+            }.resume()
+    }
+    
+    // Sign in
+    
+    func signIn(with user: User, completion: @escaping (Error?) -> ()) {
+        let signInURL = baseURL.appendingPathComponent("users/login")
+        
+        var request = URLRequest(url: signInURL)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-type")
+        
+        let jsonEncoder = JSONEncoder()
+        do {
+            let jsonData = try jsonEncoder.encode(user)
+            request.httpBody = jsonData
+        } catch {
+            print("Error encoding user object: \(error)")
+            completion(error)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                completion(NSError(domain: "", code: response.statusCode, userInfo: nil))
+                return
+            }
+            
+            if let error = error {
+                completion(error)
+                return
+            }
+            
+            guard let data = data else {
+                completion(NSError())
+                return
+            }
+            let decoder = JSONDecoder()
+            do {
+                self.bearer = try decoder.decode(Bearer.self, from: data)
+            } catch {
+                completion(error)
+                return
+            }
+            
+            completion(nil)
+            }.resume()
+    }
+    
+    
+    
+}
---
+++ ios-Gigs/View Controllers/GigsTableViewController.swift
@@ -0,0 +1,73 @@
+//
+//  GigsTableViewController.swift
+//  ios-Gigs
+//
+//  Created by Kat Milton on 6/19/19.
+//  Copyright © 2019 Kat Milton. All rights reserved.
+//
+
+import UIKit
+
+class GigsTableViewController: UITableViewController {
+    
+    var gigController = GigController()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if gigController.bearer == nil {
+            performSegue(withIdentifier: "LoginModalSegue", sender: self)
+        }
+    }
+
+    // MARK: - Table view data source
+
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "", for: indexPath)
+        
+        return cell
+    }
+    
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+
+  
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "LoginModalSegue" {
+            if let loginVC = segue.destination as? LoginViewController {
+                loginVC.gigController = gigController
+            }
+        }
+    }
+    
+
+}
