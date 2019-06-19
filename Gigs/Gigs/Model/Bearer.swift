e5e1d14478b63ec537c5a018efb8d94823bd54bb
Marlon Raskin <marlonjraskin@gmail.com>
2019-06-19 15:51:14 -0400

    Day 1: Parts 1 and 2 are done - Created the User and Bearer models, storyboard is setup.

---
+++ Gigs/Gigs/Model/Bearer.swift
@@ -0,0 +1,14 @@
+//
+//  Bearer.swift
+//  Gigs
+//
+//  Created by Marlon Raskin on 6/19/19.
+//  Copyright © 2019 Marlon Raskin. All rights reserved.
+//
+
+import Foundation
+
+
+struct Bearer: Codable {
+	var token: String
+}
