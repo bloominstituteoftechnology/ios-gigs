79b0112353f95c880532f10a6a1349e9b22c2ccf
Jeffrey Santana <SantanaJny@gmail.com>
2019-08-07 15:01:20 -0400

    Initial commit + UI

---
+++ Gigs/Gigs/Controllers/LoginVC.swift
@@ -0,0 +1,39 @@
+//
+//  LoginVC.swift
+//  Gigs
+//
+//  Created by Jeffrey Santana on 8/7/19.
+//  Copyright © 2019 Lambda. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+	
+	//MARK: - IBOutlets
+	
+	@IBOutlet weak var signTypeSegControl: UISegmentedControl!
+	@IBOutlet weak var usernameTextField: UITextField!
+	@IBOutlet weak var passwordTextField: UITextField!
+	
+	//MARK: - Properties
+	
+	
+	//MARK: - Life Cycle
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		
+	}
+	
+	//MARK: - IBActions
+	
+	@IBAction func signBtnTapped(_ sender: Any) {
+	}
+	
+	//MARK: - Helpers
+	
+
+}
+
---
+++ Gigs/Gigs/GigController.swift
@@ -0,0 +1,21 @@
+//
+//  GigController.swift
+//  Gigs
+//
+//  Created by Jeffrey Santana on 8/7/19.
+//  Copyright © 2019 Lambda. All rights reserved.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+	case get = "GET"
+	case put = "PUT"
+	case post = "POST"
+	case delete = "DELETE"
+}
+
+class GigController {
+	
+	
+}
