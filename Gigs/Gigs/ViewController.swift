388da10f563216db4f08bfe4945fd1f2cfde67fd
Alex Shillingford <alex.shillingford@icloud.com>
2019-08-07 13:46:06 -0600

    First commit

---
+++ Gigs/Gigs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gigs
+//
+//  Created by Alex Shillingford on 8/7/19.
+//  Copyright © 2019 Alex Shillingford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
