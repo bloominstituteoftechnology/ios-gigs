0f7f5b35952e3b5a15660323e82b99fa563085ac
bradleyyin <bradleyyin@gmail.com>
2019-08-08 15:41:50 -0700

    implement loading  and creating gigs

---
+++ Gigs/Gigs/Model/Gig.swift
@@ -0,0 +1,15 @@
+//
+//  Gig.swift
+//  Gigs
+//
+//  Created by Bradley Yin on 8/8/19.
+//  Copyright © 2019 bradleyyin. All rights reserved.
+//
+
+import Foundation
+
+struct Gig: Codable {
+    let title: String
+    let description: String
+    let dueDat: Date
+}
--- Gigs/Gigs/Model/GigController.swift
+++ Gigs/Gigs/Model/GigController.swift
@@ -11,7 +11,14 @@ import Foundation
 
 class GigController {
     var bearer: Bearer?
+    
+    var gigs: [Gig] = []
+    
     var baseURL = URL(string: "https://lambdagigs.vapor.cloud/api")!
+    
+    init() {
+        fetchAllGigs()
+    }
     
     func signUp(with username: String, password: String, completion: @escaping (Error?)-> Void){
         let signUpURL = baseURL.appendingPathComponent("users/signup")
@@ -80,6 +87,61 @@ class GigController {
         }.resume()
     }
     
+    func fetchAllGigs(/*completion: @escaping (Result<[Gig], NetworkError>) -> Void*/) {
+        guard let bearer = bearer  else { return }
+        let requestURL = baseURL.appendingPathComponent("gigs")
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = HTTPMethod.get.rawValue
+        request.setValue("Bearer \(bearer.token)", forHTTPHeaderField: "Authorization")
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error{
+                NSLog("Error fetching all gigs: \(error)")
+                //completion(.failure(.otherError(error)))
+            }
+            guard let data = data else{
+                NSLog("No data return on fetch all data")
+                //completion(.failure(.noData))
+                return
+            }
+            do {
+                let decoder = JSONDecoder()
+                decoder.dateDecodingStrategy = .iso8601
+                let gigs = try decoder.decode([Gig].self, from: data)
+                self.gigs = gigs
+                //completion(.success(gigs))
+            } catch {
+                NSLog("Error decoding all Gigs")
+                //completion(.failure(.noDecode))
+            }
+            
+        }
+    }
+    
+    func createGig (title: String, description: String, dueDate: Date, completion: @escaping (Result<Gig, NetworkError>) -> Void) {
+        guard let bearer = bearer else { return }
+        let requestURL = baseURL.appendingPathComponent("gigs")
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("Bearer \(bearer.token)", forHTTPHeaderField: "Authorization")
+        let gig = Gig(title: title, description: description, dueDat: dueDate)
+        let encoder = JSONEncoder()
+        encoder.dateEncodingStrategy = .iso8601
+        do {
+            let data = try encoder.encode(gig)
+            request.httpBody = data
+            URLSession.shared.dataTask(with: request) { (data, response, error) in
+                if let error = error{
+                    NSLog("Error creating gig: \(error)")
+                }
+                completion(.success(gig))
+            }.resume()
+        } catch {
+            NSLog("Error encoding gig: \(error)")
+        }
+        
+    }
+    
     enum HTTPMethod: String{
         case get = "GET"
         case put = "PUT"
@@ -87,4 +149,11 @@ class GigController {
         case delete = "DELETE"
     }
     
+}
+
+enum NetworkError: Error{
+    case noDecode
+    case noToken
+    case noData
+    case otherError(Error)
 }
