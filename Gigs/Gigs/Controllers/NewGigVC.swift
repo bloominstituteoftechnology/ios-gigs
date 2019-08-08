a10c7b604abb3f5bfec795691a4390b347832875
Jeffrey Santana <SantanaJny@gmail.com>
2019-08-08 16:08:54 -0400

    Creating ui for new gig

---
+++ Gigs/Gigs/Controllers/NewGigVC.swift
@@ -0,0 +1,38 @@
+//
+//  NewGigVC.swift
+//  Gigs
+//
+//  Created by Jeffrey Santana on 8/8/19.
+//  Copyright © 2019 Lambda. All rights reserved.
+//
+
+import UIKit
+
+class NewGigVC: UIViewController {
+	
+	//MARK: - IBOutlets
+	
+	@IBOutlet weak var titleTextField: UITextField!
+	@IBOutlet weak var datePicker: UIDatePicker!
+	@IBOutlet weak var descriptionTextView: UITextView!
+	
+	//MARK: - Properties
+	
+	
+	//MARK: - Life Cycle
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		
+	}
+	
+	//MARK: - IBActions
+	
+	@IBAction func saveBtnTapped(_ sender: Any) {
+	}
+	
+	//MARK: - Helpers
+	
+
+}
