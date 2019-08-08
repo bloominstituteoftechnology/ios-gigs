1339f765213c43fa95cfbfdb08b0851c2986cf56
Alex Shillingford <alex.shillingford@icloud.com>
2019-08-08 13:07:42 -0600

    Update Storyboard

--- Gigs/Gigs/View Controllers/LoginViewController.swift
+++ Gigs/Gigs/View Controllers/LoginViewController.swift
@@ -27,7 +27,7 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        loginButton.layer.cornerRadius = 8.0
     }
     
     @IBAction func segmentedControlChanged(_ sender: UISegmentedControl) {
