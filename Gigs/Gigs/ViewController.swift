f2fccc4e658726f31469f148fb5dcbae8c06ecf2
Angel <angelbuenrostro@outlook.com>
2019-08-24 09:18:09 -0400

    Create Xcode project

---
+++ Gigs/Gigs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gigs
+//
+//  Created by Angel Buenrostro on 8/24/19.
+//  Copyright © 2019 Angel Buenrostro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
