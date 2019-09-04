959e4ac6cd9499ae47d5ddf9a5146db42be22ca3
Andrew Ruiz <nukedoom@gmail.com>
2019-09-04 15:18:40 -0700

    Completed Part 2: Models

---
+++ iOS Gigs/Models/Bearer.swift
@@ -0,0 +1,13 @@
+//
+//  Bearer.swift
+//  iOS Gigs
+//
+//  Created by Andrew Ruiz on 9/4/19.
+//  Copyright © 2019 Andrew Ruiz. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer: Codable {
+    let token: String
+}
