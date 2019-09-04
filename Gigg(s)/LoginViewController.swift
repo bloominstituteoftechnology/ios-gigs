29b227ed77e7e23cbac53488baac06daf1e6a5db
AustinPotts <pottsevolvements@gmail.com>
2019-09-04 16:16:20 -0400

    Phase 1 of App Complete/ LogIn ViewController Code.

--- Gigg(s)/LoginViewController.swift
+++ Gigg(s)/LoginViewController.swift
@@ -26,22 +26,72 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Customize button appearance
+        signInButton.backgroundColor = UIColor(hue: 190/360, saturation: 70/100, brightness: 80/100, alpha: 1.0)
+        signInButton.tintColor = .white
+        signInButton.layer.cornerRadius = 8.0
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        guard let username = usernameTextField.text,
+            let password = passwordTextField.text,
+            !username.isEmpty,
+            !password.isEmpty else {return}
+        
+        let user = User(username: username, password: password)
+        
+        if loginType == .signUp{
+            gigController?.signUp(with: user, completion: { (networkError) in
+                
+                if let error = networkError {
+                    NSLog("Error occured during sign up: \(error)")
+                } else {
+                    let alert = UIAlertController(title: "Sign Up Success", message: "Now sign in", preferredStyle: .alert)
+                    
+                    let okAction = UIAlertAction(title: "ok", style: .default, handler: nil)
+                    
+                    alert.addAction(okAction)
+                    
+                    DispatchQueue.main.async {
+                        self.present(alert, animated: true, completion: {
+                            self.loginType = .signIn
+                            self.segmentController.selectedSegmentIndex = 1
+                            self.signInButton.setTitle("Sign In", for: .normal)
+                        })
+                        
+                    }
+                    
+                }
+                
+            })
+            
+        } else if loginType == .signIn {
+            gigController?.login(with: user, completion: { (networkError) in
+                if let error = networkError {
+                    NSLog("Error occured loging: \(error)")
+                } else{
+                    DispatchQueue.main.async {
+                        self.dismiss(animated: true, completion: nil)
+                    }
+                }
+            })
+        }
+        
     }
-    */
+    
+   
     
     
     @IBAction func signInTypeChanged(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            loginType = .signUp
+            signInButton.setTitle("Sign Up", for: .normal)
+        } else {
+            loginType = .signIn
+            signInButton.setTitle("Sign in", for: .normal)
+        }
     }
     
 }
