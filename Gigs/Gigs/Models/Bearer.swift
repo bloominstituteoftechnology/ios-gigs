f89c5da657bb5624c7483ad0ac1ce05c0902db5f
Percy <percival.ngan@gmail.com>
2019-09-04 14:54:49 -0500

    Added the models

---
+++ Gigs/Gigs/Models/Bearer.swift
@@ -0,0 +1,13 @@
+//
+//  Bearer.swift
+//  Gigs
+//
+//  Created by Percy Ngan on 9/4/19.
+//  Copyright © 2019 Lamdba School. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer: Codable {
+	let token: String
+}
---
+++ Gigs/Gigs/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Gigs
+//
+//  Created by Percy Ngan on 9/4/19.
+//  Copyright © 2019 Lamdba School. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+	let username: String
+	let password: String
+}
