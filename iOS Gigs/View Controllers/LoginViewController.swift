393f1ad632f83b263c12f4b1c2dc99d74db49ccc
Andrew Ruiz <nukedoom@gmail.com>
2019-09-04 13:47:08 -0700

    Part I Storyboard Setup complete

---
+++ iOS Gigs/View Controllers/LoginViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LoginViewController.swift
+//  iOS Gigs
+//
+//  Created by Andrew Ruiz on 9/4/19.
+//  Copyright © 2019 Andrew Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var signUpLogin: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func segmentedControlButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func signUpLoginButtonTapped(_ sender: Any) {
+    }
+    
+    
+}
