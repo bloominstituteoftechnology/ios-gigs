aec06cd5600ff33e8a1a578b672488eade89cda9
ladybeitel <ciarabeitel@gmail.com>
2019-09-04 16:15:53 -0400

    Create Bearer model and User model.

---
+++ Gigs/Gigs/Models/Bearer.swift
@@ -0,0 +1,13 @@
+//
+//  Bearer.swift
+//  Gigs
+//
+//  Created by Ciara Beitel on 9/4/19.
+//  Copyright © 2019 Ciara Beitel. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer: Codable {
+    var token: String
+}
