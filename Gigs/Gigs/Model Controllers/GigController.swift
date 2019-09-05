ffb2306e2679455f1381ac988a98f7f0f546e107
Percy <percival.ngan@gmail.com>
2019-09-04 19:25:31 -0500

    Added signup method

--- Gigs/Gigs/Model Controllers/GigController.swift
+++ Gigs/Gigs/Model Controllers/GigController.swift
@@ -8,9 +8,73 @@
 
 import Foundation
 
+enum HTTPMethod: String {
+	case get = "GET"
+	case put = "PUT"
+	case post = "POST"
+	case delete = "DELETE"
+}
+
+enum NetworkError: Error {
+	case encodingError
+	case responseError
+	case otherError
+	case noData
+	case noDecode
+}
+
 class GigController {
 
 	var bearer: Bearer?
 
-	let baseURL = URL(string: "https://lambdagigs.vapor.cloud/api")
+	let baseURL = URL(string: "https://lambdagigs.vapor.cloud/api")!
+
+	// Start of the sign up method
+	func signUp(with user: User, completion: @escaping (NetworkError?) -> Void) {
+
+		let signUpURL = baseURL.appendingPathComponent("users/signup")
+
+		var request = URLRequest(url: signUpURL)
+		request.httpMethod = HTTPMethod.post.rawValue
+		//request.setValue(<#T##value: String?##String?#>, forHTTPHeaderField: <#T##String#>)
+
+		let encoder = JSONEncoder()
+
+		do {
+			// Convert the User object into JSON data
+			let userData = try encoder.encode(user)
+
+			// Attach the user JSON to the URLRequest
+			request.httpBody = userData
+
+		} catch {
+			NSLog("Error encoding user: \(error)") //This error is the default name given to any errors
+			completion(.encodingError)
+			return
+		}
+
+		// URLSession a class used to perform network data transfer task
+		// shared is a built-in instance of URLSession
+		// The request is the URLRequest that is given to the URLSession and told to access the api
+		// The data, response, and error are just the things that are returned from the api
+		URLSession.shared.dataTask(with: request) { (data, response, error) in
+
+			if let response = response as? HTTPURLResponse,
+				response.statusCode != 200 {
+				completion(.responseError)
+				return
+			}
+
+			if let error = error {
+				NSLog("Error creating user on server: \(error)")
+				completion(.otherError)
+				return
+			}
+			completion(nil)
+		}.resume()
+	}
+	// End of the sign up method
+
+
 }
+
