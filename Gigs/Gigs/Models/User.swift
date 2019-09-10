e1fdeef189a733a7ca6dd8be09ba90881492915a
Robert Keffury <bobby.keffury@me.com>
2019-09-10 08:49:56 -0700

    Created bearer and user files with structs for each.

---
+++ Gigs/Gigs/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Gigs
+//
+//  Created by Bobby Keffury on 9/10/19.
+//  Copyright © 2019 Bobby Keffury. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    let username: String
+    let password: String
+}
