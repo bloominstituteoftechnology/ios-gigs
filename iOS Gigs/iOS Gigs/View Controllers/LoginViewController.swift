96ab4596182d39b7d351e01fe3c4df32ac5c9d46
Vici Shaweddy <vshaweddy@gmail.com>
2019-09-10 14:32:34 -0400

    Part 1 - Storyboard Setup

---
+++ iOS Gigs/iOS Gigs/View Controllers/LoginViewController.swift
@@ -0,0 +1,39 @@
+//
+//  LoginViewController.swift
+//  iOS Gigs
+//
+//  Created by Vici Shaweddy on 9/10/19.
+//  Copyright © 2019 Vici Shaweddy. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var loginTypeSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signInButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func signInTypeChanged(_ sender: Any) {
+    }
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
