b082763911c05c76282bd561a2f94dba96d0a81e
John Kouris <jdkouris@gmail.com>
2019-09-10 17:54:04 -0700

    Added code to sign up or sign in a user based on which segment they have selected on the segmentedControl

--- Gigs/Gigs/View Controllers/LoginViewController.swift
+++ Gigs/Gigs/View Controllers/LoginViewController.swift
@@ -33,9 +33,55 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func buttonTapped(_ sender: UIButton) {
+        // perform login or sign up operation based on loginType
+        guard let gigController = gigController else { return }
+        
+        if let username = usernameTextField.text,
+            !username.isEmpty,
+            let password = passwordTextField.text,
+            !password.isEmpty {
+            
+            let user = User(username: username, password: password)
+            
+            if loginType == .signUp {
+                gigController.signUp(with: user) { (error) in
+                    if let error = error {
+                        print("Error occurred during sign up: \(error.localizedDescription)")
+                    } else {
+                        DispatchQueue.main.async {
+                            let alert = UIAlertController(title: "Sign Up Successful", message: "Now please sign in.", preferredStyle: .alert)
+                            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                            alert.addAction(okAction)
+                            self.present(alert, animated: true, completion: {
+                                self.loginTypeSegmentedControl.selectedSegmentIndex = 1
+                                self.loginType = .signIn
+                                self.signInButton.setTitle("Sign In", for: .normal)
+                            })
+                        }
+                    }
+                }
+            } else if loginType == .signIn {
+                gigController.signIn(with: user) { (error) in
+                    if let error = error {
+                        print("Error occurred during sign in: \(error.localizedDescription)")
+                    } else {
+                        DispatchQueue.main.async {
+                            self.dismiss(animated: true, completion: nil)
+                        }
+                    }
+                }
+            }
+        }
     }
     
     @IBAction func signInTypeChanged(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            loginType = .signUp
+            signInButton.setTitle("Sign Up", for: .normal)
+        } else {
+            loginType = .signIn
+            signInButton.setTitle("Sing In", for: .normal)
+        }
     }
     
     
