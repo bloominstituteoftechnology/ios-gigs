9913ec6267e66595995052f9b3c6246a72551a15
dillonp23 <dillonp.lambda@gmail.com>
2019-09-10 20:18:46 -0700

    Add code to Login View Controller, prepare for segue in Table View Controller

--- Gigs/Gigs/View Controllers/LoginViewController.swift
+++ Gigs/Gigs/View Controllers/LoginViewController.swift
@@ -8,6 +8,12 @@
 
 import UIKit
 
+enum LoginType {
+    case logIn
+    case signUp
+}
+
+
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var signInSignUpSelection: UISegmentedControl!
@@ -15,7 +21,8 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var signInSignUpButton: UIButton!
     
-    
+    var gigController: GigController!
+    var loginType: LoginType?
     
     
     override func viewDidLoad() {
@@ -27,10 +34,43 @@ class LoginViewController: UIViewController {
 
     
     @IBAction func loginSignUpSelected(_ sender: Any) {
+        if signInSignUpSelection.selectedSegmentIndex == 0 {
+            loginType = .signUp
+            signInSignUpButton.setTitle("Sign Up", for: .normal)
+        } else {
+            loginType = .logIn
+            signInSignUpButton.setTitle("Sign In", for: .normal)
+        }
     }
     
     
     @IBAction func loginSignUpButton(_ sender: Any) {
+        guard let gigController = gigController else { return }
+        
+        if let username = usernameTextField.text,
+            !username.isEmpty,
+            let password = passwordTextField.text,
+            !password.isEmpty {
+            
+            let user = User(username: username, password: password)
+            
+            gigController.signUp(with: user) { (error) in
+                if let error = error {
+                    print("Error occured during sign up: \(error)")
+                } else {
+                    DispatchQueue.main.async {
+                        let alertController = UIAlertController(title: "Sign Up Successful", message: "Now please log in.", preferredStyle: .alert)
+                        let alertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                        alertController.addAction(alertAction)
+                        self.present(alertController, animated: true, completion: {
+                            self.signInSignUpSelection.selectedSegmentIndex = 1
+                            self.loginType = .logIn
+                            self.signInSignUpButton.setTitle("Sign In", for: .normal)
+                        })
+                    }
+                }
+            }
+        }
     }
     
     
