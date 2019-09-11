2ca6471a1bde56c2acc8907cd552d3b1e1e897d3
Vici Shaweddy <vshaweddy@gmail.com>
2019-09-10 22:52:31 -0400

    Part 4 - GigsTableViewController

--- iOS Gigs/iOS Gigs/View Controllers/GigsTableViewController.swift
+++ iOS Gigs/iOS Gigs/View Controllers/GigsTableViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class GigsTableViewController: UITableViewController {
+    
+    let gigController = GigController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,20 +21,22 @@ class GigsTableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if gigController.bearer == nil {
+            performSegue(withIdentifier: "LoginViewModalSegue", sender: self)
+        }
+        
+        // TODO: fetch gigs here
+    }
 
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return 0
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
@@ -40,7 +44,6 @@ class GigsTableViewController: UITableViewController {
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
@@ -77,14 +80,16 @@ class GigsTableViewController: UITableViewController {
     }
     */
 
-    /*
+
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        // Inject dependancies
+        if let loginVC = segue.destination as? LoginViewController {
+            loginVC.gigController = gigController
+        }
+        
     }
-    */
 
 }
--- iOS Gigs/iOS Gigs/View Controllers/LoginViewController.swift
+++ iOS Gigs/iOS Gigs/View Controllers/LoginViewController.swift
@@ -8,22 +8,83 @@
 
 import UIKit
 
+enum LoginType {
+    case signUp
+    case logIn
+}
+
 class LoginViewController: UIViewController {
     @IBOutlet weak var loginTypeSegmentedControl: UISegmentedControl!
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var signInButton: UIButton!
     
+    var gigController: GigController?
+    
+    var loginType = LoginType.signUp
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.signInButton.backgroundColor = UIColor(hue: 190/360, saturation: 70/100, brightness: 80/100, alpha: 1)
+        self.signInButton.tintColor = .white
+        self.signInButton.layer.cornerRadius = 8.0
 
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func signInTypeChanged(_ sender: Any) {
+    @IBAction func signInTypeChanged(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            loginType = .signUp
+            signInButton.setTitle("Sign Up", for: .normal)
+        } else if sender.selectedSegmentIndex == 1 {
+            loginType = .signUp
+            signInButton.setTitle("Log In", for: .normal)
+        }
+        
     }
     
-    @IBAction func buttonTapped(_ sender: Any) {
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        guard let gigController = gigController else { return }
+        
+        if let username = usernameTextField.text,
+               !username.isEmpty,
+           let password = passwordTextField.text,
+               !password.isEmpty {
+            
+            print(username)
+            let user = User(username: username, password: password)
+            
+            if loginType == .signUp {
+                gigController.signUp(with: user) { (error) in
+                    if let error = error {
+                        print("Error occur during sign up: \(error)")
+                    } else {
+                        DispatchQueue.main.async {
+                            let alertController = UIAlertController(title: "Sign Up Successful", message: "You can log in now", preferredStyle: .alert)
+                            let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                            
+                            alertController.addAction(alertAction)
+                            self.present(alertController, animated: true, completion: {
+                                self.loginTypeSegmentedControl.selectedSegmentIndex = 1
+                                self.loginType = .logIn
+                                self.signInButton.setTitle("Log In", for: .normal)
+                            })
+                        }
+                    }
+                }
+            } else if loginType == .logIn {
+                gigController.logIn(with: user) { (error) in
+                    if let error = error {
+                        print("Error occur during sign in: \(error)")
+                    } else {
+                        DispatchQueue.main.async {
+                            self.dismiss(animated: true, completion: nil)
+                        }
+                    }
+                }
+            }
+        }
     }
     
     /*
