02fb61a9b2cbac991101b24cef0bff729b5ed396
John Kouris <jdkouris@gmail.com>
2019-09-10 17:41:05 -0700

    Created the signIn method

--- Gigs/Gigs/Application/GigController.swift
+++ Gigs/Gigs/Application/GigController.swift
@@ -53,6 +53,52 @@ class GigController {
         }.resume()
     }
     
-    
+    // create function for sign in
+    func signIn(with user: User, completion: @escaping (Error?) -> Void) {
+        let signInUrl = baseUrl.appendingPathComponent("users/login")
+        
+        var request = URLRequest(url: signInUrl)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let jsonEncoder = JSONEncoder()
+        do {
+            let jsonData = try jsonEncoder.encode(user)
+            request.httpBody = jsonData
+        } catch {
+            print("Error encoding user object: \(error.localizedDescription)")
+            completion(error)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                completion(NSError(domain: "", code: response.statusCode, userInfo: nil))
+                return
+            }
+            
+            if let error = error {
+                completion(error)
+                return
+            }
+            
+            guard let data = data else {
+                completion(NSError())
+                return
+            }
+            
+            let jsonDecoder = JSONDecoder()
+            do {
+                self.bearer = try jsonDecoder.decode(Bearer.self, from: data)
+            } catch {
+                print("Error decoding bearer object: \(error.localizedDescription)")
+                completion(error)
+                return
+            }
+            
+            completion(nil)
+        }.resume()
+    }
     
 }
