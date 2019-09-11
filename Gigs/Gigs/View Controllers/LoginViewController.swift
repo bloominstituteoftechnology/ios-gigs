fa19e99fe1c0f0633ceb46bd53c7721f89d96a7c
John Kouris <jdkouris@gmail.com>
2019-09-10 17:48:02 -0700

    Added skeleton code for GigsTableViewController to function Began working on the LoginViewController

--- Gigs/Gigs/View Controllers/LoginViewController.swift
+++ Gigs/Gigs/View Controllers/LoginViewController.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+enum LoginType {
+    case signUp
+    case signIn
+}
+
 class LoginViewController: UIViewController {
     
     @IBOutlet weak var usernameTextField: UITextField!
@@ -15,11 +20,16 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var loginTypeSegmentedControl: UISegmentedControl!
     @IBOutlet weak var signInButton: UIButton!
     
+    var gigController: GigController?
+    var loginType = LoginType.signUp
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        signInButton.backgroundColor = UIColor(hue: 190/360, saturation: 70/100, brightness: 80/100, alpha: 1)
+        signInButton.tintColor = .white
+        signInButton.layer.cornerRadius = 8.0
     }
     
     @IBAction func buttonTapped(_ sender: UIButton) {
