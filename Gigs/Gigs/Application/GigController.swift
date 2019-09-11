1c887f71492bc31cc45777f1ceed455de53c13f1
dillonp23 <dillonp.lambda@gmail.com>
2019-09-10 19:42:32 -0700

    Add sign up and sign in function to GigController

---
+++ Gigs/Gigs/Application/GigController.swift
@@ -0,0 +1,116 @@
+//
+//  GigController.swift
+//  Gigs
+//
+//  Created by Dillon P on 9/10/19.
+//  Copyright © 2019 Lambda iOSPT2. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+}
+
+class GigController {
+    
+    var bearer: Bearer?
+    private let baseURL: URL = URL(string: "https://lambdagigs.vapor.cloud/api")!
+    
+    
+    // sign-up functionality
+    
+    func signUp(with user: User, completion: @escaping (Error?) -> Void) {
+        let signUpUrl = baseURL.appendingPathComponent("users/signup")
+        
+        
+        var request = URLRequest(url: signUpUrl)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let jsonEncoder = JSONEncoder()
+        
+        do {
+            let jsonData = try jsonEncoder.encode(user)
+            request.httpBody = jsonData
+        } catch {
+            print("Error encoding user object: \(error)")
+            completion(error)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { (_, response, error) in
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                completion(NSError(domain: "", code: response.statusCode, userInfo: nil))
+                return
+            }
+            
+            if let error = error {
+                completion(error)
+                return
+            }
+            
+            completion(nil)
+            
+        }.resume()
+    }
+    
+    
+    // sign-in function functionality
+    
+    func signIn(with user: User, completion: @escaping (Error?) -> Void) {
+        let signInURL = baseURL.appendingPathComponent("users/login")
+        
+        var request = URLRequest(url: signInURL)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let jsonEncoder = JSONEncoder()
+        
+        do {
+            let jsonData = try jsonEncoder.encode(user)
+            request.httpBody = jsonData
+        } catch {
+            print("Error encoding user object: \(error)")
+            completion(error)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                completion(NSError(domain: "", code: response.statusCode, userInfo: nil))
+                return
+            }
+            
+            if let error = error {
+                completion(error)
+                return
+            }
+            
+            guard let data = data else {
+                completion(NSError())
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            do {
+                self.bearer = try decoder.decode(Bearer.self, from: data)
+            } catch {
+                print("Error decoding bearer object: \(error)")
+                completion(error)
+                return
+            }
+            
+            completion(nil)
+        }.resume()
+        
+    }
+    
+    
+    
+    
+}
