a3becbb550b2f2b6e62d832c63224b0526d5c41a
Robert Keffury <bobby.keffury@me.com>
2019-09-12 12:21:57 -0700

    Added view controller to storyboard and view controller file.

---
+++ Gigs/Gigs/View Controllers/GigDetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  GigDetailViewController.swift
+//  Gigs
+//
+//  Created by Bobby Keffury on 9/12/19.
+//  Copyright © 2019 Bobby Keffury. All rights reserved.
+//
+
+import UIKit
+
+class GigDetailViewController: UIViewController {
+    
+    @IBOutlet weak var gigTitleTextField: UITextField!
+    @IBOutlet weak var gigDescriptionTextView: UITextView!
+    @IBOutlet weak var gigDatePicker: UIDatePicker!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+    }
+    
+
+}
