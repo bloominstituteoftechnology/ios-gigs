cd2e9a4cb557e5c451745bd143dcd3ecaf48597d
Jessie Griffin <i.jessie.me@icloud.com>
2019-09-12 00:01:58 -0400

    Day 2 - Part 1 - Storyboard Addition finished except for Actions and Outlets

---
+++ Gigs/Gigs/View Controllers/GigsDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GigsDetailViewController.swift
+//  Gigs
+//
+//  Created by Jessie Ann Griffin on 9/11/19.
+//  Copyright © 2019 Jessie Griffin. All rights reserved.
+//
+
+import UIKit
+
+class GigsDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
