070eff41bbd3dfa0e175f8dda38b406a76b53808
Jessie Griffin <i.jessie.me@icloud.com>
2019-09-12 21:47:28 -0400

    Part 1 - Storyboard Additions complete

--- Gigs/Gigs/View Controllers/GigsDetailViewController.swift
+++ Gigs/Gigs/View Controllers/GigsDetailViewController.swift
@@ -10,21 +10,20 @@ import UIKit
 
 class GigsDetailViewController: UIViewController {
 
+    // MARK: IBOutlets
+    @IBOutlet weak var jobTitleTextField: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+    // MARK: Properties
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func saveGig(_ sender: Any) {
     }
-    */
-
+    
 }
