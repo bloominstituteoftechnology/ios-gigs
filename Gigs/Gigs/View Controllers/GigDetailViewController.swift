0e1f0c53729b3e28f19463b6cd3125c3858a7313
dillonp23 <dillonp.lambda@gmail.com>
2019-09-12 19:01:36 -0700

    Add GigDetailViewController and UI

---
+++ Gigs/Gigs/View Controllers/GigDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GigDetailViewController.swift
+//  Gigs
+//
+//  Created by Dillon P on 9/12/19.
+//  Copyright © 2019 Lambda iOSPT2. All rights reserved.
+//
+
+import UIKit
+
+class GigDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
