d0237414c0d7dc63b1ff0736422a00c7161f0812
Isvvc <lyons@tuta.io>
2019-10-02 13:58:59 -0600

    Add GigController with signUp function

---
+++ Gigs/Gigs/GigController.swift
@@ -0,0 +1,61 @@
+//
+//  GigController.swift
+//  Gigs
+//
+//  Created by Isaac Lyons on 10/2/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+}
+
+class GigController {
+    var bearer: Bearer?
+    let baseURL = URL(string: "https://lambdagigs.vapor.cloud/api")!
+    
+    func signUp(with user: User, completion: @escaping (Error?) -> Void) {
+        // Build the URL
+        
+        let requestURL = baseURL
+            .appendingPathComponent("users")
+            .appendingPathComponent("signup")
+        
+        // Build the request
+        
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        do {
+            let userJSON = try JSONEncoder().encode(user)
+            request.httpBody = userJSON
+        } catch {
+            NSLog("Error encoding user data: \(error)")
+            completion(error)
+            return
+        }
+        
+        // Perform the request
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                NSLog("Error signing up user: \(error)")
+                completion(error)
+                return
+            }
+            
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                let statusCodeError = NSError(domain: "com.lambdaschool.gigs", code: response.statusCode, userInfo: nil)
+                completion(statusCodeError)
+                return
+            }
+            
+            completion(nil)
+        }.resume()
+    }
+}
