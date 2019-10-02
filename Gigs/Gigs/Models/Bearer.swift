8fb49263171db76e01bcb38e14edecb09f64b2cc
Jesse Ruiz <jesseleeruiz@gmail.com>
2019-10-02 09:21:03 -1000

    Finished Part 2

---
+++ Gigs/Gigs/Models/Bearer.swift
@@ -0,0 +1,13 @@
+//
+//  Bearer.swift
+//  Gigs
+//
+//  Created by Jesse Ruiz on 10/2/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer: Codable {
+    var token: String
+}
---
+++ Gigs/Gigs/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Gigs
+//
+//  Created by Jesse Ruiz on 10/2/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    var username: String
+    var password: String
+}
