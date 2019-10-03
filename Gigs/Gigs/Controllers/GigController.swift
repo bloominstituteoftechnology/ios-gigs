4069651ae7da3f096b527dbc9db8f65a5ee22310
Jesse Ruiz <jesseleeruiz@gmail.com>
2019-10-03 10:28:10 -1000

    Finished Part 3 in Day 2

--- Gigs/Gigs/Controllers/GigController.swift
+++ Gigs/Gigs/Controllers/GigController.swift
@@ -15,11 +15,25 @@ enum HTTPMethod: String {
     case delete = "DELETE"
 }
 
+enum NetworkingError: Error {
+    case noData
+    case noBearer
+    case serverError(Error)
+    case unexpectedStatusCode
+    case badDecode
+}
+
+enum HeaderNames: String {
+    case authorization = "Authorization"
+    case contentType = "Content-Type"
+}
+
 class GigController {
     
     // MARK: - Properties
     var bearer: Bearer?
     private let baseURL = URL(string: "https://lambdagigs.vapor.cloud/api")!
+    var gigs: [Gig] = []
     
     // CREATE FUNC FOR SIGNUP
     func signUp(with user: User, completion: @escaping (Error?) -> ()) {
@@ -117,4 +131,94 @@ class GigController {
             completion(nil)
             }.resume()
     }
+    
+    func fetchAllGigs(completion: @escaping (Result<[String], NetworkingError>) -> ()) {
+        
+        guard let bearer = bearer else {
+            completion(.failure(.noBearer))
+            return
+        }
+        
+        let requestURL = baseURL
+            .appendingPathComponent("gigs")
+            .appendingPathComponent("all")
+        
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = HTTPMethod.get.rawValue
+        request.setValue("Bearer \(bearer.token)", forHTTPHeaderField: HeaderNames.authorization.rawValue)
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            if let error = error {
+                completion(.failure(.serverError(error)))
+                return
+            }
+            
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                completion(.failure(.unexpectedStatusCode))
+            }
+            
+            guard let data = data else {
+                completion(.failure(.noData))
+                return
+            }
+            
+            do {
+                let gigNames = try JSONDecoder().decode([String].self, from: data)
+                completion(.success(gigNames))
+            } catch {
+                NSLog("Error decoding gig names: \(error)")
+                completion(.failure(.badDecode))
+            }
+        }.resume()
+    }
+    
+    func fetchDetails(for gigName: String, completion: @escaping (Result<Gig, NetworkingError>) -> ()) {
+        
+        guard let bearer = bearer else {
+            completion(.failure(.noBearer))
+            return
+        }
+        
+        let requestURL = baseURL
+            .appendingPathComponent("gigs")
+            .appendingPathComponent(gigName)
+        
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = HTTPMethod.get.rawValue
+        request.setValue("Bearer \(bearer.token)", forHTTPHeaderField: HeaderNames.authorization.rawValue)
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            if let error = error {
+                NSLog("Error fetching gig details: \(error)")
+                completion(.failure(.serverError(error)))
+            }
+            
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                completion(.failure(.unexpectedStatusCode))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(.noData))
+                return 
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.dateDecodingStrategy = .iso8601
+                
+                let gig = try decoder.decode(Gig.self, from: data)
+                
+                completion(.success(gig))
+                
+            } catch {
+                completion(.failure(.badDecode))
+            }
+        }.resume()
+    }
+    
 }
