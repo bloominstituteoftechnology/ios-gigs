d53a361f025c35785013dc785faf2347cc836597
Jesse Ruiz <jesseleeruiz@gmail.com>
2019-10-03 09:53:47 -1000

    Finished Part 1 in Day 2

---
+++ Gigs/Gigs/View Controllers/GigDetailViewController.swift
@@ -0,0 +1,28 @@
+//
+//  GigDetailViewController.swift
+//  Gigs
+//
+//  Created by Jesse Ruiz on 10/3/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class GigDetailViewController: UIViewController {
+
+    @IBOutlet weak var jobTitleText: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
+    }
+    
+
+
+}
