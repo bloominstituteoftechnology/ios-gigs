e4b4b9a65520c4c171209b41c2bac3eab9fdc86a
Rwoltx <richardx1016@gmail.com>
2019-10-30 19:40:47 -0400

    Part 2 - Models completed

---
+++ Gigs/Gigs/Base.lproj/Bearer.swift
@@ -0,0 +1,15 @@
+//
+//  Bearer.swift
+//  Gigs
+//
+//  Created by Rick Wolter on 10/30/19.
+//  Copyright © 2019 Richar Wolter. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer: Codable{
+    
+    let token: String
+    
+}
