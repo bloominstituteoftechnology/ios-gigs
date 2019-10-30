3f2d4c16c92cadaf7e57cb1866d7af60444a1e25
Dan M <djmthedev@gmail.com>
2019-10-30 13:15:15 -0700

    implemented prepare(forSegue:)

--- Gigs/Gigs/View Controllers/GigsTableViewController.swift
+++ Gigs/Gigs/View Controllers/GigsTableViewController.swift
@@ -84,14 +84,17 @@ class GigsTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == PropertyKeys.loginSegue {
+            if let loginVC = segue.destination as? LoginViewController {
+                loginVC.gigController = gigController
+            }
+        }
     }
-    */
+    
 
 }
