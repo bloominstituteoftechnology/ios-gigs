73409eea853ff29495451a280830c8ab1d21c2c8
Jon Bash <jonbashmusic@gmail.com>
2019-10-30 12:36:48 -0700

    create Bearer struct

---
+++ Gigs/Gigs/Bearer.swift
@@ -0,0 +1,15 @@
+//
+//  Bearer.swift
+//  Gigs
+//
+//  Created by Jon Bash on 2019-10-30.
+//  Copyright © 2019 Jon Bash. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer {
+    let id: Int
+    let token: String
+    let userID: Int
+}
