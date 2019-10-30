b98a2ed9575d40d5b1e7d8df013a68696474c788
Nar Kumar <njn.kumar@gmail.com>
2019-10-30 13:16:59 -0700

    Storyboard Complete

---
+++ Gigs/Gigs/View Controllers/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  Gigs
+//
+//  Created by Niranjan Kumar on 10/30/19.
+//  Copyright © 2019 Nar Kumar. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
