d0f22f6b7dee33a47b08db4887aba142e7f0c285
Brandi <blubrandi0@gmail.com>
2019-10-30 15:03:43 -0600

    Created User model

---
+++ iOS Gigs/iOS Gigs/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  iOS Gigs
+//
+//  Created by Brandi on 10/30/19.
+//  Copyright © 2019 Brandi. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    let username: String
+    let password: String
+}
