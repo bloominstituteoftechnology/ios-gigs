7a7c9f5a4e4a1a660ae1dcc130a1275085a5d568
Skyler Suzuki <skylersuzuk1@gmail.com>
2019-10-30 12:28:54 -0700

    Storyboard setup. GigTableViewController and LoginViewController wired up

---
+++ Gigs/Gigs/View Controllers/LoginViewController.swift
@@ -0,0 +1,40 @@
+//
+//  LoginViewController.swift
+//  Gigs
+//
+//  Created by Lambda_School_Loaner_204 on 10/30/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var signUpInSegementedControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func signInChanged(_ sender: UISegmentedControl) {
+    }
+    
+    @IBAction func logInButtonTapped(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
