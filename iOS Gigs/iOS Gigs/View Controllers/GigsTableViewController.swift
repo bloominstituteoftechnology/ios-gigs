8a48a5c41ce9bf6f8c44104ac7cbd3983da0947e
Brandi <blubrandi0@gmail.com>
2019-10-30 16:55:32 -0600

    Added prepareForSegue, returned 0 for numberOfRowsInSetction, added instructed code for the cellForRowAt method.

--- iOS Gigs/iOS Gigs/View Controllers/GigsTableViewController.swift
+++ iOS Gigs/iOS Gigs/View Controllers/GigsTableViewController.swift
@@ -32,25 +32,21 @@ class GigsTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return 0
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "", for: indexPath)
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
@@ -87,14 +83,14 @@ class GigsTableViewController: UITableViewController {
     }
     */
 
-    /*
+
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "LoginViewModalSegue" {
+            // inject dependencies
+            // TODO: fetch gigs here
+        }
     }
-    */
 
 }
