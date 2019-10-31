b6e999d96fd766ff4e47c9ba8a9a4991a834ab6c
Jon Bash <jonbashmusic@gmail.com>
2019-10-31 12:43:23 -0700

    add gig struct

---
+++ Gigs/Gigs/Models/Gig.swift
@@ -0,0 +1,15 @@
+//
+//  Gig.swift
+//  Gigs
+//
+//  Created by Jon Bash on 2019-10-31.
+//  Copyright © 2019 Jon Bash. All rights reserved.
+//
+
+import Foundation
+
+struct Gig: Codable {
+    var title: String
+    var dueDate: Date
+    var description: String
+}
