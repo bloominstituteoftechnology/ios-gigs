fdf3e4442c0a8dd011bdac6f023c5b113c81680a
Jon Bash <jonbashmusic@gmail.com>
2019-10-31 14:07:16 -0700

    implement detailVC updateViews()

--- Gigs/Gigs/View Controllers/GigDetailViewController.swift
+++ Gigs/Gigs/View Controllers/GigDetailViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class GigDetailViewController: UIViewController {
     
+    var gigController: GigController!
+    var gig: Gig? {
+        didSet {
+            updateViews()
+        }
+    }
+    
     @IBOutlet weak var titleField: UITextField!
     @IBOutlet weak var dueDatePicker: UIDatePicker!
     @IBOutlet weak var descriptionView: UITextView!
@@ -19,5 +26,16 @@ class GigDetailViewController: UIViewController {
     }
     
     @IBAction func saveTapped(_ sender: UIBarButtonItem) {
+    }
+    
+    func updateViews() {
+        if let gig = gig {
+            title = gig.title
+            titleField.text = gig.title
+            descriptionView.text = gig.description
+            dueDatePicker.date = gig.dueDate
+        } else {
+            title = "New Gig"
+        }
     }
 }
