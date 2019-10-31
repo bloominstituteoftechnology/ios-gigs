70cf0372f068901c31d80c2acc0932ba2864ec3b
Skyler Suzuki <skylersuzuk1@gmail.com>
2019-10-31 13:43:57 -0700

    Part 3 Gig controller URLSession added for getting all gigs and creating a gig

--- Gigs/Gigs/GigController.swift
+++ Gigs/Gigs/GigController.swift
@@ -13,9 +13,19 @@ enum HTTPMethod: String {
     case post = "POST"
 }
 
+enum NetworkError: Error {
+    case noAuth
+    case badAuth
+    case otherError
+    case badData
+    case noDecode
+    case noEncode
+}
+
 class GigController {
     
     private let baseURL = URL(string: "https://lambdagigs.vapor.cloud/api")!
+    var gigs: [Gig] = []
     var bearer: Bearer?
     
     // MARK: - Sign Up/In Functions
@@ -92,6 +102,99 @@ class GigController {
             } catch {
                 print("Error decoding bearer object: \(error)")
                 completion(error)
+                return
+            }
+            
+            completion(nil)
+        }.resume()
+    }
+    
+    func getAllGigs(completion: @escaping (NetworkError?) -> Void) {
+        guard let bearer = bearer else {
+            completion(.noAuth)
+            return
+        }
+        
+        let allGigsURL = baseURL.appendingPathComponent("gigs/")
+        
+        var request = URLRequest(url: allGigsURL)
+        request.httpMethod = HTTPMethod.get.rawValue
+        request.addValue("Bearer \(bearer.token)", forHTTPHeaderField: "Authorization")
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let response = response as? HTTPURLResponse,
+                response.statusCode == 401 {
+                completion(.badAuth)
+                return
+            }
+            
+            if let error = error {
+                print("Error receiving gig name data: \(error)")
+                completion(.otherError)
+            }
+            
+            guard let data = data else {
+                completion(.badData)
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            do {
+                self.gigs = try decoder.decode([Gig].self, from: data)
+                completion(nil)
+            } catch {
+                print("Error decoding gig objects: \(error)")
+                completion(.noDecode)
+            }
+        }.resume()
+    }
+    
+    func createGig(with gig: Gig, completion: @escaping (NetworkError?) -> Void) {
+        guard let bearer = bearer else {
+            completion(.noAuth)
+            return
+        }
+        
+        let addGigURL = baseURL.appendingPathComponent("gigs/")
+        
+        var request = URLRequest(url: addGigURL)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.addValue("Bearer \(bearer.token)", forHTTPHeaderField: "Authorization")
+        
+        let jsonEncoder = JSONEncoder()
+        do {
+            let jsonData = try jsonEncoder.encode(gig)
+            request.httpBody = jsonData
+        } catch {
+            print("Error encoding user object: \(error)")
+            completion(.noEncode)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let response = response as? HTTPURLResponse,
+                response.statusCode == 401 {
+                completion(.badAuth)
+                return
+            }
+            
+            if let error = error {
+                print("Error receiving gig name data: \(error)")
+                completion(.otherError)
+            }
+            
+            guard let data = data else {
+                completion(.badData)
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            do {
+                let createdGig = try decoder.decode(Gig.self, from: data)
+                self.gigs.append(createdGig)
+            } catch {
+                print("Error decoding gig object: \(error)")
+                completion(.noDecode)
                 return
             }
             
---
+++ Gigs/Gigs/Models/Gig.swift
@@ -0,0 +1,15 @@
+//
+//  Gig.swift
+//  Gigs
+//
+//  Created by Lambda_School_Loaner_204 on 10/31/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Gig: Codable {
+    let title: String
+    let dueDate: Date
+    let description: String
+}
