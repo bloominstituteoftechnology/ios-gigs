38caecab5c05b846bf87cd9314f732cab1db3d0d
Thomas Sabino-Benowitz <sabinowitz@gmail.com>
2019-10-31 22:23:15 -0400

    Commit 2: Controller complete

---
+++ ios gigs/ios gigs/GigDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GigDetailViewController.swift
+//  ios gigs
+//
+//  Created by Thomas Sabino-Benowitz on 10/31/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class GigDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
