f5d61cfea5f0b8b6e0bdbdb1c07ade574be345fe
Christian Lorenzo <clbmiami2004@gmail.com>
2019-11-05 21:23:32 -0800

    Created functions for sing in and sing up.

--- Gigs/GigController.swift
+++ Gigs/GigController.swift
@@ -12,4 +12,96 @@ class GigController: Codable {
     
     var bearer: Bearer?
     let baseUrl: URL
+    
+    
+    //Function for Sign Up and Sign In:
+    
+    func signUp(with user: User, completion: @escaping (Error?) -> ()) {
+        let signUpUrl1 = baseUrl.appendingPathComponent("users/signup")
+        
+        var request = URLRequest(url: signUpUrl1)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let jsonEncoder = JSONEncoder()
+        do {
+            let jsonData = try jsonEncoder.encode(user)
+            request.httpBody = jsonData
+        }catch {
+            print("Error encoding user object: \(error)")
+            completion(error)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { (_, response, error) in
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                completion(NSError(domain: "", code: response.statusCode, userInfo: nil))
+                return
+            }
+            
+            if let error = error {
+                completion(error)
+                return
+            }
+            
+            completion(nil)
+            
+        }.resume()
+        
+    }
+    
+    
+    //Create function for Sign in:
+    
+    func signIn(with user: User, completion: @escaping (Error?) -> ()) {
+        let loginUrl = baseUrl.appendingPathComponent("users/login")
+        
+        var request = URLRequest(url: loginUrl)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let jsonEncoder = JSONEncoder()
+        do {
+            let jsonData = try jsonEncoder.encode(user)
+            request.httpBody = jsonData
+        }catch {
+            print("Error encoding user object: \(error)")
+            completion(error)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                completion(NSError(domain: "", code: response.statusCode, userInfo: nil))
+                return
+            }
+            
+            if let error = error {
+                completion(error)
+                return
+            }
+            
+            guard let data = data else {
+                completion(NSError())
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            do {
+                self.bearer = try decoder.decode(Bearer.self, from: data)
+            }catch {
+                print("Error decoding bearer object: \(error)")
+                completion(error)
+                return
+            }
+            
+            completion(nil)
+            
+        }.resume()
+        
+    }
+    
+    
 }
