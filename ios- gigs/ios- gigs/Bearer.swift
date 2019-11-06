688e2d0bcbeaba19924c7b313d3db5e5f9c48dbf
Nicolas Rios <nickrios33@yahoo.com>
2019-11-05 16:15:54 -0800

    changed viewcontroller color and added a struct

--- ios- gigs/ios- gigs/Bearer.swift
+++ ios- gigs/ios- gigs/Bearer.swift
@@ -1,9 +1,14 @@
 //
 //  Bearer.swift
-//  AnimalSpotter
+//  ios- gigs
 //
-//  Created by Nicolas Rios on 11/3/19.
-//  Copyright © 2019 Lambda School. All rights reserved.
+//  Created by Nicolas Rios on 11/5/19.
+//  Copyright © 2019 Nicolas Rios. All rights reserved.
 //
 
 import Foundation
+
+struct Bearer:Codable {
+    var bearer:String
+    
+}
