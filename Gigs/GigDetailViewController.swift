0ca9faa581e24459452e99e40e90b2b14e914db2
Christian Lorenzo <clbmiami2004@gmail.com>
2019-11-07 20:28:41 -0800

    Created UI for the app. Created Cocoa Touch document called GigDetailViewController .

---
+++ Gigs/GigDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GigDetailViewController.swift
+//  Gigs
+//
+//  Created by Lambda_School_Loaner_201 on 11/7/19.
+//  Copyright © 2019 Christian Lorenzo. All rights reserved.
+//
+
+import UIKit
+
+class GigDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
