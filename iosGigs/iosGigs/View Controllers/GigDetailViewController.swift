2c89cacc8a579ac72a6e59ba1cd876d8c1d1a012
Denis Cedeno <denis.cedeno@gmail.com>
2019-11-07 19:46:00 -0500

    created storyboard additions

---
+++ iosGigs/iosGigs/View Controllers/GigDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GigDetailViewController.swift
+//  iosGigs
+//
+//  Created by denis cedeno on 11/7/19.
+//  Copyright © 2019 DenCedeno Co. All rights reserved.
+//
+
+import UIKit
+
+class GigDetailViewController: UIViewController {
+
+    
+    @IBOutlet weak var jobTitleTextField: UITextField!
+    
+    @IBOutlet weak var dueDatePicker: UIDatePicker!
+    
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+    @IBAction func saveAction(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
