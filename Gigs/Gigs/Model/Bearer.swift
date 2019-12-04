ec7759630c9827dcf0da62fcfd339f3f77d38096
Chad Rutherford <chad_rutherford@outlook.com>
2019-12-04 14:56:45 -0500

    Completed Part 2 - Models

---
+++ Gigs/Gigs/Model/Bearer.swift
@@ -0,0 +1,13 @@
+//
+//  Bearer.swift
+//  Gigs
+//
+//  Created by Chad Rutherford on 12/4/19.
+//  Copyright © 2019 lambdaschool.com. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer: Codable {
+    let token: String
+}
