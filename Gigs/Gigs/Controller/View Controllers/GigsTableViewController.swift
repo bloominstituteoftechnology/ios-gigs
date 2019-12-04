3705ad5ccec9cbff1bdb6ccec755171ef1b5970f
Chad Rutherford <chad_rutherford@outlook.com>
2019-12-04 17:25:50 -0500

    Finished Project

--- Gigs/Gigs/Controller/View Controllers/GigsTableViewController.swift
+++ Gigs/Gigs/Controller/View Controllers/GigsTableViewController.swift
@@ -10,4 +10,39 @@ import UIKit
 
 class GigsTableViewController: UITableViewController {
     
+    let authController = AuthenticationController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if authController.bearer == nil {
+            performSegue(withIdentifier: "ShowLoginSegue", sender: self)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier {
+        case "ShowLoginSegue":
+            guard let loginVC = segue.destination as? LoginViewController else { return }
+            loginVC.authController = authController
+        default:
+            break
+        }
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "", for: indexPath)
+        return cell
+    }
 }
--- Gigs/Gigs/Controller/View Controllers/LoginViewController.swift
+++ Gigs/Gigs/Controller/View Controllers/LoginViewController.swift
@@ -8,12 +8,20 @@
 
 import UIKit
 
+enum LoginType {
+    case signUp
+    case logIn
+}
+
 class LoginViewController: UIViewController {
     
     @IBOutlet weak var loginTypeSegmentedControl: UISegmentedControl!
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var signInButton: UIButton!
+    
+    var authController: AuthenticationController!
+    var loginType: LoginType = .signUp
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,10 +31,46 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func signInButtonTapped(_ sender: UIButton) {
+        guard let authController = authController else { return }
+        guard let username = usernameTextField.text, !username.isEmpty, let password = passwordTextField.text, !password.isEmpty else { return }
+        let user = User(username: username, password: password)
         
+        switch loginType {
+        case .signUp:
+            authController.signUp(with: user) { error in
+                if let error = error {
+                    print("Error occurred during sign up: \(error)")
+                } else {
+                    let alertController = UIAlertController(title: "Sign Up Successful", message: "Please log in now", preferredStyle: .alert)
+                    alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                    self.present(alertController, animated: true) {
+                        self.loginType = .logIn
+                        self.loginTypeSegmentedControl.selectedSegmentIndex = 1
+                        self.signInButton.setTitle("Log In", for: .normal)
+                    }
+                }
+            }
+        case .logIn:
+            authController.logIn(with: user) { error in
+                if let error = error {
+                    print("Error occurred during log in: \(error)")
+                } else {
+                    self.dismiss(animated: true, completion: nil)
+                }
+            }
+        }
     }
     
     @IBAction func loginTypeChanged(_ sender: UISegmentedControl) {
-        
+        switch sender.selectedSegmentIndex {
+        case 0:
+            loginType = .signUp
+            signInButton.setTitle("Sign Up", for: .normal)
+        case 1:
+            loginType = .logIn
+            signInButton.setTitle("Log In", for: .normal)
+        default:
+            break
+        }
     }
 }
