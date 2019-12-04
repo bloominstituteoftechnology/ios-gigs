e554dcff7b071f2bbe5f765d64a1f71c65c1bb76
Zack Larsen <zacklarsen1@gmail.com>
2019-12-04 13:14:47 -0700

    part 1 finished

---
+++ Gigs/Gigs/LoginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LoginViewController.swift
+//  Gigs
+//
+//  Created by Zack Larsen on 12/4/19.
+//  Copyright © 2019 Zack Larsen. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var segmentedButton: UISegmentedControl!
+    @IBOutlet weak var usernameText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var button: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
