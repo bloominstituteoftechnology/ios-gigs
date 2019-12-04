ff489ee36c115bfd2121eaf46499ef36cb1c2d57
Chad Rutherford <chad_rutherford@outlook.com>
2019-12-04 14:33:16 -0500

    Initial Commit and Project Restructure

---
+++ Gigs/Gigs/Supporting Files/Constants/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Gigs
+//
+//  Created by Chad Rutherford on 12/4/19.
+//  Copyright © 2019 lambdaschool.com. All rights reserved.
+//
+
+import Foundation
+
+struct Segues {
+    
+}
