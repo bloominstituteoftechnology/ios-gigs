acf9b29f279f8d2ec1b614260f3079e691cc3bca
Zack Larsen <zacklarsen1@gmail.com>
2019-12-04 14:08:10 -0700

    part 3 done, working on view controllers

---
+++ Gigs/Gigs/GigController.swift
@@ -0,0 +1,100 @@
+//
+//  GigController.swift
+//  Gigs
+//
+//  Created by Zack Larsen on 12/4/19.
+//  Copyright © 2019 Zack Larsen. All rights reserved.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+}
+
+class GigController {
+    
+    var bearer: Bearer?
+    private let baseURL = URL(string: "https://lambdagigs.vapor.cloud/api")!
+    
+    func signUP(with user: User, completion: @escaping (Error?) -> ()) {
+        let signUPUrl = baseURL.appendingPathComponent("users/signup")
+        
+        var request = URLRequest(url: signUPUrl)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let jsonEncoder = JSONEncoder()
+        do {
+            let jsonData = try jsonEncoder.encode(user)
+            request.httpBody = jsonData
+        } catch {
+            print("Error encoding user object: \(error)")
+            completion(error)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) {_, response, error in
+            if let error = error {
+                completion(error)
+                return
+            }
+            if let response = response as? HTTPURLResponse, response.statusCode != 200 {
+                completion(NSError(domain: "", code: response.statusCode, userInfo: nil))
+                return
+            }
+            
+            DispatchQueue.main.async {
+                completion(nil)
+            }
+        }.resume()
+    }
+    
+    func signIn(with user: User, completion: @escaping (Error?) -> ()) {
+        let signInUrl = baseURL.appendingPathComponent("users/login")
+        
+        var request = URLRequest(url: signInUrl)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        let jsonEncoder = JSONEncoder()
+        do {
+            let jsonData = try jsonEncoder.encode(user)
+            request.httpBody = jsonData
+        } catch {
+            print("Error encoding user object: \(error)")
+            completion(error)
+            return
+        }
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(error)
+                return
+            }
+            
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                completion(NSError(domain: "", code: response.statusCode, userInfo: nil))
+                return
+            }
+            
+            guard let data = data else {
+                completion(NSError())
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            do {
+                self.bearer = try decoder.decode(Bearer.self, from: data)
+            } catch {
+                print("Error decoding bearer object: \(error)")
+                completion(error)
+                return
+            }
+            
+            DispatchQueue.main.async {
+                completion(nil)
+            }
+        }.resume()
+    }
+}
---
+++ Gigs/Gigs/Models/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  Gigs
+//
+//  Created by Zack Larsen on 12/4/19.
+//  Copyright © 2019 Zack Larsen. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    
+let username: String
+let password: String
+    
+}
