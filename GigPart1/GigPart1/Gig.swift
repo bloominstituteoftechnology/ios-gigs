8fd238ffd5d62d87e3fd5516edc26ab9e03b4477
Tyler Christian <tylerc71197@gmail.com>
2019-12-05 16:35:46 -0500

    added the get and post logic

---
+++ GigPart1/GigPart1/Gig.swift
@@ -0,0 +1,15 @@
+//
+//  Gig.swift
+//  GigPart1
+//
+//  Created by Lambda_School_Loaner_218 on 12/5/19.
+//  Copyright © 2019 Lambda_School_Loaner_218. All rights reserved.
+//
+
+import Foundation
+
+struct Gig: Codable {
+    let title: String
+    let dueDate: Date
+    let description: String
+}
--- GigPart1/GigPart1/GigController.swift
+++ GigPart1/GigPart1/GigController.swift
@@ -15,8 +15,18 @@ enum HTTPMethod: String {
     case delete = "Delete"
 }
 
+enum NetworkError: Error {
+    case noAuth
+    case badAuth
+    case otherError
+    case badData
+    case noDecode
+    case noEncode
+}
+
 class GigController {
     
+    var gigs: [Gig] = []
     var bearer: Bearer?
     let baseURL = URL(string: "https://lambdagigs.vapor.cloud/api")
     
@@ -102,6 +112,88 @@ class GigController {
             
             DispatchQueue.main.async {
                 completion(nil)
+            }
+        }.resume()
+    }
+    // Get all gigs /gigs/
+    func getallGigs(completion: @escaping (Result<[Gig], NetworkError>) -> ()) {
+        guard let url = baseURL else { return }
+        guard let bearer = bearer else {
+            completion(.failure(.noAuth))
+            return
+        }
+        
+        let allGigsURL = url.appendingPathComponent("gigs")
+        var request = URLRequest(url: allGigsURL)
+        request.httpMethod = HTTPMethod.get.rawValue
+        request.setValue("Bearer \(bearer.token)", forHTTPHeaderField: "Authorization")
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let response = response as? HTTPURLResponse, response.statusCode == 401 {
+                completion(.failure(.badAuth))
+                return
+            }
+            
+            if let error = error {
+                print("Error receving Gig data: \(error)")
+                completion(.failure(.otherError))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(.badData))
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            do {
+                let results = try decoder.decode([Gig].self, from: data)
+                DispatchQueue.main.async {
+                    completion(.success(results))
+                }
+            } catch let decodeError {
+                print("Error decoding gigs from data: \(decodeError)")
+                completion(.failure(.noDecode))
+                return
+            }
+        }.resume()
+    }
+    
+    // Post gigs /gigs/
+    
+    func createNewGig(with gig: Gig, completion: @escaping (Result<Gig, NetworkError>) -> ()) {
+        guard let url = baseURL else { return }
+        guard let bearer = bearer else {
+            completion(.failure(.noAuth))
+            return
+        }
+        let postGigURL = url.appendingPathComponent("gigs")
+        var request = URLRequest(url: postGigURL)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("Bearer \(bearer.token)", forHTTPHeaderField: "Authorization")
+        
+        URLSession.shared.dataTask(with: request) { _, response, error in
+            if let response = response as? HTTPURLResponse, response.statusCode == 401 {
+                completion(.failure(.badAuth))
+                return
+            }
+            
+            if let error = error {
+                print("Error posting gig: \(error)")
+                completion(.failure(.otherError))
+                return
+            }
+            
+            let encoder = JSONEncoder()
+            do {
+                let data = try encoder.encode(gig)
+                request.httpBody = data
+                completion(.success(gig))
+            } catch let encoderError {
+                print("Error encoding gig object: \(encoderError)")
+                completion(.failure(.noEncode))
+                return 
             }
         }.resume()
     }
