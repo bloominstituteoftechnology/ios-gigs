32bde1586096e232c05e6fa5cb7fc2c4f4e89e35
Tyler Christian <tylerc71197@gmail.com>
2019-12-05 15:52:46 -0500

    added detialViewController

---
+++ GigPart1/GigPart1/GigDetialViewController.swift
@@ -0,0 +1,27 @@
+//
+//  GigDetialViewController.swift
+//  GigPart1
+//
+//  Created by Lambda_School_Loaner_218 on 12/5/19.
+//  Copyright © 2019 Lambda_School_Loaner_218. All rights reserved.
+//
+
+import UIKit
+
+class GigDetialViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var descriptionView: UITextView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
+    }
+    
+}
