85d6ba68d0b1eb1ae1243decc0e02411eb1ce7e2
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-01-15 13:36:40 -0800

    Part 2 done

---
+++ Gigs/Gigs/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Gigs
+//
+//  Created by Jorge Alvarez on 1/15/20.
+//  Copyright © 2020 Jorge Alvarez. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    let username: String
+    let password: String
+}
