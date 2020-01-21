a36b2ff7b4ae2030fea62b075eaaa640c14707d4
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-01-21 17:21:09 -0500

    Initial Commit

---
+++ Gigs/Gigs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gigs
+//
+//  Created by Joshua Rutkowski on 1/21/20.
+//  Copyright © 2020 Josh Rutkowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
