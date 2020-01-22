46fec3edcef874e017b6d1e92b77937e7e3f363c
morgan smith <mssmitty07@gmail.com>
2020-01-21 22:53:55 -0500

    finished with part 1

---
+++ Gigs/Gigs/Bearer.swift
@@ -0,0 +1,13 @@
+//
+//  Bearer.swift
+//  Gigs
+//
+//  Created by Morgan Smith on 1/21/20.
+//  Copyright © 2020 Morgan Smith. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer: Codable {
+    let token: String
+}
---
+++ Gigs/Gigs/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Gigs
+//
+//  Created by Morgan Smith on 1/21/20.
+//  Copyright © 2020 Morgan Smith. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    var username: String
+    var password: String
+}
