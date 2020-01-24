aaaf49e3f8f5a873fc1e1d129e59977ed371b9f2
David Wright <davidtwright89@gmail.com>
2020-01-23 23:26:11 -0500

    Finished Part 3: Finished implementing GigController

--- Gigs/Gigs/Models/GigController.swift
+++ Gigs/Gigs/Models/GigController.swift
@@ -14,6 +14,15 @@ enum HTTPMethod: String {
     case post = "POST"
 }
 
+enum NetworkError: Error {
+    case noAuth
+    case badAuth
+    case otherError
+    case badData
+    case decodingError
+    case encodingError
+}
+
 class GigController {
     
     // MARK: - Properties
@@ -22,6 +31,8 @@ class GigController {
     var bearer: Bearer?
     private let baseUrl = URL(string: "https://lambdagigs.vapor.cloud/api")!
     
+    // MARK: - Sign Up
+
     func signUp(with user: User, completion: @escaping (Error?) -> ()) {
         let signUpURL = baseUrl.appendingPathComponent("users/signup")
         
@@ -55,6 +66,8 @@ class GigController {
         }.resume()
     }
     
+    // MARK: - Sign In
+
     func signIn(with user: User, completion: @escaping (Error?) -> ()) {
         let signInURL = baseUrl.appendingPathComponent("users/login")
         
@@ -99,6 +112,92 @@ class GigController {
             }
             
             completion(nil)
+        }.resume()
+    }
+    
+    // MARK: - Fetch All Gigs
+
+    func fetchAllGigs(completion: @escaping (Result<[Gig], NetworkError>) -> Void) {
+        guard let bearer = bearer else {
+            completion(.failure(.noAuth))
+            return
+        }
+        
+        let allGigsUrl = baseUrl.appendingPathComponent("gigs")
+        
+        var request = URLRequest(url: allGigsUrl)
+        request.httpMethod = HTTPMethod.get.rawValue
+        request.setValue("Bearer \(bearer.token)", forHTTPHeaderField: "Authorization")
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print("Error receiving gigs data: \(error)")
+                completion(.failure(.otherError))
+                return
+            }
+            
+            if let response = response as? HTTPURLResponse,
+                response.statusCode == 401 {
+                completion(.failure(.badAuth))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(.badData))
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            do {
+                let allGigs = try decoder.decode([Gig].self, from: data)
+                self.gigs = allGigs
+                completion(.success(allGigs))
+            } catch {
+                print("Error decoding gig objects: \(error)")
+                completion(.failure(.decodingError))
+            }
+        }.resume()
+    }
+    
+    // MARK: - Create Gig
+
+    func createGig(_ gig: Gig, completion: @escaping (Result<Gig, NetworkError>) -> Void) {
+        guard let bearer = bearer else {
+            completion(.failure(.noAuth))
+            return
+        }
+        
+        let createGigUrl = baseUrl.appendingPathComponent("gigs")
+        
+        var request = URLRequest(url: createGigUrl)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("Bearer \(bearer.token)", forHTTPHeaderField: "Authorization")
+        
+        let jsonEncoder = JSONEncoder()
+        do {
+            let jsonData = try jsonEncoder.encode(gig)
+            request.httpBody = jsonData
+        } catch {
+            print("Error encoding user object: \(error)")
+            completion(.failure(.encodingError))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { _, response, error in
+            if let error = error {
+                print("Error receiving gigs data: \(error)")
+                completion(.failure(.otherError))
+                return
+            }
+            
+            if let response = response as? HTTPURLResponse,
+                response.statusCode == 401 {
+                completion(.failure(.badAuth))
+                return
+            }
+            
+            self.gigs.append(gig)
+            completion(.success(gig))
         }.resume()
     }
 }
