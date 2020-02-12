9ba2d7f9975b683eb0bb116107561aff4171cfff
scottharris86 <scottharrisdev@gmail.com>
2020-02-12 18:13:28 -0500

    completed sign in and sign up workflow. users can successfully sign in and sign up

--- Gigs/Gigs/View Controllers/GigsTableViewController.swift
+++ Gigs/Gigs/View Controllers/GigsTableViewController.swift
@@ -10,4 +10,35 @@ import UIKit
 
 class GigsTableViewController: UITableViewController {
     
+    var gigController = GigController()
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if gigController.bearer == nil {
+            performSegue(withIdentifier: "LoginModalSegue", sender: self)
+        } else {
+            // TODO: fetch gigs here
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "GigCell", for: indexPath)
+        
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "LoginModalSegue" {
+            if let loginVC = segue.destination as? LoginViewController {
+                loginVC.gigController = gigController
+            }
+        }
+    }
+    
+    
+    
 }
