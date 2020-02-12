46bd805005669971c377eb6a2373fe634e94cf88
Chris <chris.gonzales.82@gmail.com>
2020-02-12 12:49:23 -0800

    created User and Bearer models

---
+++ Gigs/Gigs/Model/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  Gigs
+//
+//  Created by Chris Gonzales on 2/12/20.
+//  Copyright © 2020 Chris Gonzales. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    
+    let username: String
+    let password: String
+    
+}
---
+++ Gigs/Gigs/ViewControllers/Bearer.swift
@@ -0,0 +1,14 @@
+//
+//  Bearer.swift
+//  Gigs
+//
+//  Created by Chris Gonzales on 2/12/20.
+//  Copyright © 2020 Chris Gonzales. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer: Codable{
+    
+    let token: String
+}
