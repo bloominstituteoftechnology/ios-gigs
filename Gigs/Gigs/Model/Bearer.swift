5f99c13cfcfe87ccc3c28d35f8ea8d21d44660c1
Enrique Gongora <EGTech24@gmail.com>
2020-02-12 13:32:47 -0800

    Models added

---
+++ Gigs/Gigs/Model/Bearer.swift
@@ -0,0 +1,13 @@
+//
+//  Bearer.swift
+//  Gigs
+//
+//  Created by Enrique Gongora on 2/12/20.
+//  Copyright © 2020 Enrique Gongora. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer: Codable {
+    let token: String
+}
