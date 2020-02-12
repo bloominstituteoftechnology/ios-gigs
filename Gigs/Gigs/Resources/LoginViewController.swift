9016f6ec5b7e75d986ba869e6909b1abad6989e3
scottharris86 <scottharrisdev@gmail.com>
2020-02-12 16:36:56 -0500

    completed storyboard ui layout and connections

---
+++ Gigs/Gigs/Resources/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginViewController.swift
+//  Gigs
+//
+//  Created by scott harris on 2/12/20.
+//  Copyright © 2020 scott harris. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var loginTypeSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signInButton: UIButton!
+    
+    @IBAction func loginTypeChanged(_ sender: UISegmentedControl) {
+        
+    }
+    
+    @IBAction func signInTapped(_ sender: UIButton) {
+        
+    }
+    
+}
---
+++ Gigs/Gigs/View Controllers/GigsTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  GigsTableViewController.swift
+//  Gigs
+//
+//  Created by scott harris on 2/12/20.
+//  Copyright © 2020 scott harris. All rights reserved.
+//
+
+import UIKit
+
+class GigsTableViewController: UITableViewController {
+    
+}
