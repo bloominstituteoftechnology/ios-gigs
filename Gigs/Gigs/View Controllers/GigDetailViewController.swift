5b76441491068adf4a7ce990787ae6a785d68791
scottharris86 <scottharrisdev@gmail.com>
2020-02-13 18:10:06 -0500

    added detail view controller and attached actions and outlets

---
+++ Gigs/Gigs/View Controllers/GigDetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  GigDetailViewController.swift
+//  Gigs
+//
+//  Created by scott harris on 2/13/20.
+//  Copyright © 2020 scott harris. All rights reserved.
+//
+
+import UIKit
+
+class GigDetailViewController: UIViewController {
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var dueDatePicker: UIDatePicker!
+    @IBOutlet weak var descriptionTextField: UITextView!
+    
+    
+    @IBAction func saveTapped(_ sender: Any) {
+    }
+    
+}
