1e321eee153c9104ce65bb1a07d710e2fc850831
Hector L <hectoremil21@hotmail.com>
2020-03-11 15:36:00 -0400

    Create User model.

---
+++ Gigs/Gigs/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Gigs
+//
+//  Created by Karen Rodriguez on 3/11/20.
+//  Copyright © 2020 Hector Ledesma. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    var username: String
+    var password: String
+}
