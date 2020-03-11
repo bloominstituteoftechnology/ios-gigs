fc6fa9f8544a4485399157a2fe71fcb50d895cc3
Hector L <hectoremil21@hotmail.com>
2020-03-11 17:06:29 -0400

    Implement login view controller button and segment methods.

--- Gigs/Gigs/View Controllers/LoginViewController.swift
+++ Gigs/Gigs/View Controllers/LoginViewController.swift
@@ -8,12 +8,21 @@
 
 import UIKit
 
+enum LoginType {
+    case signUp
+    case logIn
+}
+
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var segmentControl: UISegmentedControl!
     @IBOutlet weak var usernameField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var userSubmitButton: UIButton!
+    
+    var gigController: GigController?
+    var loginType: LoginType = .signUp
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -32,8 +41,53 @@ class LoginViewController: UIViewController {
     */
 
     @IBAction func segmentChange(_ sender: Any) {
+        if segmentControl.selectedSegmentIndex == 0 {
+            self.loginType = .signUp
+            userSubmitButton.setTitle("Sign Up", for: .normal)
+        } else {
+            self.loginType = .logIn
+            userSubmitButton.setTitle("Log In", for: .normal)
+        }
     }
     @IBAction func userSubmitTapped(_ sender: Any) {
+        if let username = usernameField.text,
+            !username.isEmpty,
+            let password = passwordField.text,
+            !password.isEmpty  {
+            let user = User(username: username, password: password)
+            
+            if self.loginType == .signUp {
+                gigController?.signUp(with: user, completion: { error in
+                    if let error = error {
+                        NSLog("Error when pressing sign up button: \(error)")
+                        return
+                    } else {
+                        DispatchQueue.main.async {
+                            let alert = UIAlertController(title: "Sign Up Successful", message: "You can now Log In", preferredStyle: .alert)
+                            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                            alert.addAction(okAction)
+                            
+                            self.present(alert, animated: true) {
+                                self.loginType = .logIn
+                                self.segmentControl.selectedSegmentIndex = 1
+                                self.userSubmitButton.setTitle("Log In", for: .normal)
+                            }
+                        }
+                    }
+                })
+            } else {
+                gigController?.logIn(with: user, completion: { error in
+                    if let error = error {
+                        NSLog("Error occurred during log in: \(error)")
+                    } else {
+                        DispatchQueue.main.async {
+                            self.dismiss(animated: true, completion: nil)
+                        }
+                    }
+                })
+            }
+        }
+        
     }
     
 }
