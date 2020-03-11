aebabb54ea5d0e8462e33605fd1119eddc03d97b
Bharat Kumar <Bharatiosdeveloper@gmail.com>
2020-03-11 16:11:58 -0700

    Initializing the project

---
+++ Gigs/Gigs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gigs
+//
+//  Created by Bhawnish Kumar on 3/11/20.
+//  Copyright © 2020 Bhawnish Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
