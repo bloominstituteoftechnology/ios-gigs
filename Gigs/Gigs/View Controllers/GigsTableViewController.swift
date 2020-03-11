c533f98fc310c0615a8cea26aa5fe835883c4b63
Hector L <hectoremil21@hotmail.com>
2020-03-11 16:43:08 -0400

    Programmatic segue implemented

--- Gigs/Gigs/View Controllers/GigsTableViewController.swift
+++ Gigs/Gigs/View Controllers/GigsTableViewController.swift
@@ -9,17 +9,21 @@
 import UIKit
 
 class GigsTableViewController: UITableViewController {
+    
+    var gigController = GigController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        if gigController.bearer == nil {
+            performSegue(withIdentifier: "AuthenticateSegue", sender: self)
+        } else {
+            // TODO: fetch gigs here
+        }
+    }
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
