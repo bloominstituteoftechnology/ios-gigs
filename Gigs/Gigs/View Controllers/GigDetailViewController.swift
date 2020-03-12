7a8d6861938b27fe5fbdc124a4ea7af736b948bb
Wyatt Harrell <wyattaharrell@gmail.com>
2020-03-12 15:16:54 -0400

    part 1 complete

---
+++ Gigs/Gigs/View Controllers/GigDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GigDetailViewController.swift
+//  Gigs
+//
+//  Created by Wyatt Harrell on 3/12/20.
+//  Copyright © 2020 Wyatt Harrell. All rights reserved.
+//
+
+import UIKit
+
+class GigDetailViewController: UIViewController {
+
+    @IBOutlet weak var jobTitleTextField: UITextField!
+    @IBOutlet weak var dateDatePicker: UIDatePicker!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+    }
+    
+
+    
+
+}
