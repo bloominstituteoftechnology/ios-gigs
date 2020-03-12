ab1f824301b2b6a3aafbb8a8d3c3d09ddfe0bb99
Nichole Davidson <nicholedavid@gmail.com>
2020-03-12 16:32:56 -0600

    Added segues

--- Gigs/Gigs/ViewController/GigsTableViewController.swift
+++ Gigs/Gigs/ViewController/GigsTableViewController.swift
@@ -10,18 +10,18 @@ import UIKit
 
 class GigsTableViewController: UITableViewController {
     
-//
-//    @IBOutlet weak var gigTitleLabel: UILabel!
-//    @IBOutlet weak var dueDateLabel: UILabel!
+    //
+    //    @IBOutlet weak var gigTitleLabel: UILabel!
+    //    @IBOutlet weak var dueDateLabel: UILabel!
     
     var gigController = GigController()
     var gig: Gig!
     let df = DateFormatter()
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-     
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -31,42 +31,57 @@ class GigsTableViewController: UITableViewController {
             // TODO: fetch gigs here
         }
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 0
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return gigController.gigs.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "GigCell", for: indexPath)
-
+        
         // Configure the cell...
         cell.textLabel?.text = gigController.gigs[indexPath.row].title
         df.dateStyle = .short
         cell.detailTextLabel?.text = df.string(from: gig.dueDate)
-   
+        
         return cell
     }
     
     // MARK: - Navigation
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-           if segue.identifier == "LoginViewModalSegue" {
-               // inject dependencies
-               if let loginVC = segue.destination as? LoginViewController {
-                   loginVC.gigController = gigController
-               }
-           } else {
-            if segue.identifier == ""
+        if segue.identifier == "LoginViewModalSegue" {
+            // inject dependencies
+            if let loginVC = segue.destination as? LoginViewController {
+                loginVC.gigController = gigController
+            }
+        } else {
+            if segue.identifier == "AddGigSegue" {
+                if let addGigVC = segue.destination as? GigDetailViewController {
+                    addGigVC.gigController = gigController
+                    addGigVC.datePicker.date = gig.dueDate
+                    addGigVC.jobTitleTextField.text = gig.title
+                    addGigVC.jobDescriptionTextView.text = gig.description
+                } else {
+                    if segue.identifier == "ShowGigSegue" {
+                        if let gigDetailVC = segue.destination as? GigDetailViewController {
+                            gigDetailVC.gigController = gigController
+                            gigDetailVC.datePicker.date = gig.dueDate
+                            gigDetailVC.jobDescriptionTextView.text = gig.description
+                            gigDetailVC.jobTitleTextField.text = gig.title
+                        }
+                    }
+                }
+            }
         }
-       }
-
+    }
 }
