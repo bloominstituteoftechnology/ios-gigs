85c2bf734d17499ed545a55f6ce44f56febab946
Mark Gerrior <github@gerrior.com>
2020-03-12 15:29:58 -0700

    Part 1 complete

--- Gigs/Gigs/View Controllers/GigDetailViewController.swift
+++ Gigs/Gigs/View Controllers/GigDetailViewController.swift
@@ -9,22 +9,23 @@
 import UIKit
 
 class GigDetailViewController: UIViewController {
+    
+    // MARK: - Properites
 
+    // MARK: - Outlets
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+    // MARK: - Actions
+
+    @IBAction func saveButton(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
