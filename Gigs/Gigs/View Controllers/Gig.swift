057c87d3db6ccff87a39d5a7cc86d177f24ed2dc
Bharat Kumar <Bharatiosdeveloper@gmail.com>
2020-03-12 14:01:18 -0700

    Created the gig struct for the gig.swift

---
+++ Gigs/Gigs/View Controllers/Gig.swift
@@ -0,0 +1,15 @@
+//
+//  Gig.swift
+//  Gigs
+//
+//  Created by Bhawnish Kumar on 3/12/20.
+//  Copyright © 2020 Bhawnish Kumar. All rights reserved.
+//
+
+import Foundation
+
+struct Gig {
+    let title: String
+    let dueDate: DateFormatter
+    let description: String
+}
