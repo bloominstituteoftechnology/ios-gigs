bbd473c5a5e3eaad792c1d5e81a7c742fa03323d
Mark Gerrior <github@gerrior.com>
2020-03-12 16:14:18 -0700

    Completed Part 2

---
+++ Gigs/Gigs/Models/Gig.swift
@@ -0,0 +1,15 @@
+//
+//  Gig.swift
+//  Gigs
+//
+//  Created by Mark Gerrior on 3/12/20.
+//  Copyright © 2020 Mark Gerrior. All rights reserved.
+//
+
+import Foundation
+
+struct Gig {
+    var title: String
+    var description: String
+    var dueDate: Date
+}
