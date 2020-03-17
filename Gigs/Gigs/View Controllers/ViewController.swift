b29da7e4be52f357aca1e4ac5a9fe8b108dac345
Claudia Maciel <clc_80@yahoo.com>
2020-03-17 16:24:23 -0700

    Initial Commit

---
+++ Gigs/Gigs/View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gigs
+//
+//  Created by Claudia Contreras on 3/17/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
