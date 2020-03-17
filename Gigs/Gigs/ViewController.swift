4c946a8e8c0bf51dc1c97d745fc3713f96a62503
Juan M Mariscal <j.m.mariscal03@gmail.com>
2020-03-16 20:46:53 -0700

    Created XCode Project

---
+++ Gigs/Gigs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gigs
+//
+//  Created by Juan M Mariscal on 3/16/20.
+//  Copyright © 2020 Juan M Mariscal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
