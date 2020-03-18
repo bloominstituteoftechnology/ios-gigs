f6180755415aa546c6a38459d9efcf0a9765cfbd
David Williams <burmesedragon@att.net>
2020-03-17 18:30:12 -0700

    finished setting up part 1 - need to find why the alert doesn't show

--- Gigs/Gigs/LoginViewController.swift
+++ Gigs/Gigs/LoginViewController.swift
@@ -13,38 +13,83 @@ enum LoginType {
     case signIn
 }
 class LoginViewController: UIViewController {
-
-    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var loginTypeSegmentedControl: UISegmentedControl!
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var signInButton: UIButton!
     
     var loginType = LoginType.signUp
+    var gigController: GigController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
     @IBAction func loginSignupButtonPressed(_ sender: Any) {
+        guard let gigController = gigController else { return }
         
+        guard let username = usernameTextField.text,
+            username.isEmpty == false,
+            let password = passwordTextField.text,
+            password.isEmpty == false else {
+                return
+        }
+        
+        let user = User(username: username, password: password)
+        
+        switch loginType {
+        case .signUp:
+            gigController.signUp(with: user) { (error) in
+                guard error == nil else {
+                    print("Error signing up: \(error!)")
+                    return
+                }
+                
+                DispatchQueue.main.async {
+                    let alertController = UIAlertController(title: "Sign Up Successful", message: "Please Log-In", preferredStyle: .alert)
+                    let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                    alertController.addAction(alertAction)
+                    
+                    self.present(alertController, animated: true) {
+                        self.loginType = .signIn
+                        self.loginTypeSegmentedControl
+                            .selectedSegmentIndex = 1
+                        self.signInButton.setTitle("Sign In", for: .normal)
+                    }
+                }
+            }
+        case .signIn:
+            gigController.signIn(with: user) { (error) in
+                guard error == nil else {
+                    print("Error logging in: \(error!)")
+                    return
+                }
+                
+                DispatchQueue.main.async {
+                    self.dismiss(animated: true, completion: nil)
+                }
+            }
+        }
     }
+    
     @IBAction func segmentedControl(_ sender: UISegmentedControl) {
         if sender.selectedSegmentIndex == 0 {
             loginType = .signUp
-            signInButton.setTitle("sign Up", for: .normal)
+            signInButton.setTitle("Sign Up", for: .normal)
         } else {
             loginType = .signIn
             signInButton.setTitle("Sign In", for: .normal)
