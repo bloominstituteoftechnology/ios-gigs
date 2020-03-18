3a0ac0ea307acb65616e1f01b6568dc96c70bb6b
Claudia Maciel <clc_80@yahoo.com>
2020-03-17 17:30:53 -0700

    Bearer and User swift models files created

---
+++ Gigs/Gigs/Model/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Gigs
+//
+//  Created by Claudia Contreras on 3/17/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    let username: String
+    let password: String
+}
