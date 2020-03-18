89efdf1723ad280c8480fe11fd3c087fedb39fc7
kensam1 <samarakodykennedy@gmail.com>
2020-03-17 22:52:31 -0400

    Set up storyboard and some code. Need to be finished

---
+++ Gigs/Gigs/GigController.swift
@@ -0,0 +1,17 @@
+//
+//  GigController.swift
+//  Gigs
+//
+//  Created by Lambda_School_Loaner_241 on 3/17/20.
+//  Copyright © 2020 Lambda_School_Loaner_241. All rights reserved.
+//
+
+import Foundation
+
+class GigController {
+    var bearer: Bearer?
+    let baseURL: URL
+    
+    
+    // part 3, number 2
+}
---
+++ Gigs/Gigs/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Gigs
+//
+//  Created by Lambda_School_Loaner_241 on 3/17/20.
+//  Copyright © 2020 Lambda_School_Loaner_241. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    let username: String
+    let password: String
+}
