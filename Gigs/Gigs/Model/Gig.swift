e6c76c1ec21f724e89b05c71055fb26e9b3154e3
Claudia Maciel <clc_80@yahoo.com>
2020-03-19 18:06:25 -0700

    Gig Model added

---
+++ Gigs/Gigs/Model/Gig.swift
@@ -0,0 +1,15 @@
+//
+//  Gig.swift
+//  Gigs
+//
+//  Created by Claudia Contreras on 3/19/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+import Foundation
+
+struct Gig: Codable {
+    let title: String
+    let description: String
+    let dueDate: Date
+}
