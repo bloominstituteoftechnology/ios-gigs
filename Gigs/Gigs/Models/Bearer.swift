a99f675b43aca19d73d68811545bf557f9e67c85
Jarren Campos <jarrenmcampos@gmail.com>
2020-03-19 19:16:15 -0700

    Almost Done Fixing sign up bug

---
+++ Gigs/Gigs/Models/Bearer.swift
@@ -0,0 +1,13 @@
+//
+//  Bearer.swift
+//  Gigs
+//
+//  Created by Jarren Campos on 3/17/20.
+//  Copyright © 2020 Jarren Campos. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer: Codable {
+    let token: String
+}
---
+++ Gigs/Gigs/VIew Controllers/GigsTableViewController.swift
@@ -0,0 +1,46 @@
+//
+//  GigsTableViewController.swift
+//  Gigs
+//
+//  Created by Jarren Campos on 3/17/20.
+//  Copyright © 2020 Jarren Campos. All rights reserved.
+//
+
+import UIKit
+
+class GigsTableViewController: UITableViewController {
+
+    var gigController = GigController()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if gigController.bearer == nil {
+            performSegue(withIdentifier: "LoginViewModalSegue", sender: self)
+        }
+    }
+
+    // MARK: - Table view data source
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "", for: indexPath)
+
+        return cell
+    }
+
+}
