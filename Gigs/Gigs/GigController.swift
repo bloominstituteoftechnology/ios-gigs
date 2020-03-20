eb31b922ac02e85e62998f53c703f7e509bab227
kensam1 <samarakodykennedy@gmail.com>
2020-03-20 13:15:20 -0400

    Finished the first part

--- Gigs/Gigs/GigController.swift
+++ Gigs/Gigs/GigController.swift
@@ -8,10 +8,53 @@
 
 import Foundation
 
+enum HTTPMethod: String {
+case get = "GET"
+case post = "POST"
+}
+
 class GigController {
     var bearer: Bearer?
-    let baseURL: URL
+    let baseUrl = URL(string: "https://lambdagigapi.herokuapp.com/api")!
+    
+    func signUp(with user: User, completion: @escaping (Error?)->Void){
+        
+        let signUpUrl = baseUrl.appendingPathComponent("users/signup")
+        
+        
+        // URL request
+        var request = URLRequest(url: signUpUrl)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        // encode the user to put in our body
+        let jsonEncoder = JSONEncoder()
+        do {
+            let jsonData = try jsonEncoder.encode(user)
+            request.httpBody = jsonData
+        } catch {
+            print(" Error encoding the user object: \(error)")
+            completion(error)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { (_, response, error) in
+            guard error == nil else {
+                completion(error)
+                return
+            }
+            
+            if let response = response as? HTTPURLResponse,
+                    response.statusCode != 200 {
+                    completion(NSError(domain: "", code: response.statusCode, userInfo: nil))
+                    return
+                }
+                
+                completion(nil)
+            }.resume()
+        
+    }
     
     
-    // part 3, number 2
+    
 }
