89653e5676bb11a9cde83f63f631a5c30b5f5c6a
Chris Dobek <dobek.christoher@gmail.com>
2020-04-07 16:05:32 -0400

    Storyboard of Gigs Project Set Up

---
+++ Gigs/Gigs/View Controllers/LoginViewController.swift
@@ -0,0 +1,48 @@
+//
+//  LoginViewController.swift
+//  Gigs
+//
+//  Created by Chris Dobek on 4/7/20.
+//  Copyright © 2020 Chris Dobek. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+    @IBOutlet weak var loginTypeSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func loginTypeChanged(_ sender: Any) {
+    }
+    
+    @IBAction func submitButtonTapped(_ sender: Any) {
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
