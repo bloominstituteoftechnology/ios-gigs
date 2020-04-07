f83431c3509a25c5fc5f3fe065144c19f1c3ea6a
Harmony Radley <harmonyradley99@gmail.com>
2020-04-07 16:22:23 -0400

    Part 3. Created a bearer property and a baseURL constant.

---
+++ Gigs/Gigs/Controller/GigController.swift
@@ -0,0 +1,23 @@
+//
+//  GigController.swift
+//  Gigs
+//
+//  Created by Harmony Radley on 4/7/20.
+//  Copyright © 2020 Harmony Radley. All rights reserved.
+//
+
+import Foundation
+
+
+class GigController {
+    
+    static var bearer: Bearer?
+    
+    private let baseURL = URL(string: "https://lambdagigapi.herokuapp.com/api")!
+    
+    
+    
+    
+    
+    
+}
