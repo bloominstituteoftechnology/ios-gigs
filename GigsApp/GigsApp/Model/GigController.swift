965f70a31f04c139d1a4f5716d6811eb850ea645
Bharat Kumar <Bharatiosdeveloper@gmail.com>
2020-04-07 13:32:12 -0700

    Added request method for post

--- GigsApp/GigsApp/Model/GigController.swift
+++ GigsApp/GigsApp/Model/GigController.swift
@@ -10,10 +10,25 @@ import Foundation
 
 final class GigController {
     
+    enum HTTPMethod: String {
+          case get = "GET"
+          case post = "POST"
+      }
+      enum NetworkError: Error {
+          case failedSignUp, failedSignIn, noData, badData
+      }
+    
     private static var gigUrl: GigUrl?
     static var bearer: Bearer?
     
     private var baseURL = URL(string: "https://lambdagigapi.herokuapp.com/api")!
     
     
+    
+    private func postRequest(url: URL) -> URLRequest {
+        var request = URLRequest(url: url)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        return request
+    }
 }
