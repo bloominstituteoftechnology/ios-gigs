84c5c58029eb1f3aac1668f5098df9ead6c89344
Chris Dobek <dobek.christoher@gmail.com>
2020-04-07 16:13:25 -0400

    Models Finished (Part 2)

---
+++ Gigs/Gigs/Models/Bearer.swift
@@ -0,0 +1,13 @@
+//
+//  Bearer.swift
+//  Gigs
+//
+//  Created by Chris Dobek on 4/7/20.
+//  Copyright © 2020 Chris Dobek. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer: Codable {
+    let token: String
+}
---
+++ Gigs/Gigs/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Gigs
+//
+//  Created by Chris Dobek on 4/7/20.
+//  Copyright © 2020 Chris Dobek. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    let username: String
+    let password: String
+}
