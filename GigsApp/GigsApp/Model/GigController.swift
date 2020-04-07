c8981db57454b258beb0b64779890909f007f457
Bharat Kumar <Bharatiosdeveloper@gmail.com>
2020-04-07 14:40:30 -0700

    Added sign up methodmethod to the gig controller

--- GigsApp/GigsApp/Model/GigController.swift
+++ GigsApp/GigsApp/Model/GigController.swift
@@ -7,22 +7,61 @@
 //
 
 import Foundation
+import UIKit
 
-final class GigController {
+class GigController {
     
     enum HTTPMethod: String {
-          case get = "GET"
-          case post = "POST"
-      }
-      enum NetworkError: Error {
-          case failedSignUp, failedSignIn, noData, badData
-      }
+        case get = "GET"
+        case post = "POST"
+    }
+    enum NetworkError: Error {
+        case failedSignUp, failedSignIn, noData, badData
+    }
     
-    private static var gigUrl: GigUrl?
     static var bearer: Bearer?
     
-    private var baseURL = URL(string: "https://lambdagigapi.herokuapp.com/api")!
+    private let baseURL = URL(string: "https://lambdagigapi.herokuapp.com/api")!
     
+    
+    func signUp(with user: User, completion: @escaping (Result<Bool, NetworkError>) -> Void) {
+        var signUpUrl = baseURL.appendingPathComponent("/users/signup")
+        var request = postRequest(url: signUpUrl)
+        // MARK: Encoder
+        let jsonEncoder: JSONEncoder = {
+            let encoder = JSONEncoder()
+            encoder.outputFormatting = .prettyPrinted
+            return encoder
+        }()
+        do {
+            let jsonData = try jsonEncoder.encode(user)
+            print(String(data: jsonData, encoding: .utf8)!)
+            request.httpBody = jsonData
+            URLSession.shared.dataTask(with: request) { _, response, error in
+                if let error = error {
+                    print("Error in signing up \(error.localizedDescription)")
+                    completion(.failure(.failedSignUp))
+                    return
+                }
+                guard let response = response as? HTTPURLResponse,
+                    response.statusCode == 200
+                    else {
+                        print("Unsuccessful Sign up")
+                        return completion(.failure(.failedSignUp))
+                }
+            }
+        } catch {
+            print("Error in signin up: \(error.localizedDescription)")
+            completion(.failure(.failedSignUp))
+            
+        }
+        
+    }
+    
+    
+    
+    
+    private lazy var jsonDecoder = JSONDecoder()
     
     
     private func postRequest(url: URL) -> URLRequest {
@@ -31,4 +70,5 @@ final class GigController {
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         return request
     }
+    
 }
