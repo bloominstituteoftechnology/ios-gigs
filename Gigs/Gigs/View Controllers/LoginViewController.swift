bc2a6479f39229654ad612ab6e0cba43dc723b75
Harmony Radley <harmonyradley99@gmail.com>
2020-04-07 17:57:35 -0400

    Almost completed part 4 step 1

--- Gigs/Gigs/View Controllers/LoginViewController.swift
+++ Gigs/Gigs/View Controllers/LoginViewController.swift
@@ -18,8 +18,7 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        signInButton.layer.cornerRadius = 8 
     }
     
 
