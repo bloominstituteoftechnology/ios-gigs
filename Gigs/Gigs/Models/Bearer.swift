8f59c544d424e5075a82365aac5f7a421a1898ff
Cameron Collins <cameroncollins19@yahoo.com>
2020-04-07 14:44:58 -0500

    Added Part 2

---
+++ Gigs/Gigs/Models/Bearer.swift
@@ -0,0 +1,14 @@
+//
+//  Bearer.swift
+//  Gigs
+//
+//  Created by Cameron Collins on 4/7/20.
+//  Copyright © 2020 Cameron Collins. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer: Codable {
+    let token: String
+}
+
---
+++ Gigs/Gigs/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  username.swift
+//  Gigs
+//
+//  Created by Cameron Collins on 4/7/20.
+//  Copyright © 2020 Cameron Collins. All rights reserved.
+//
+
+import Foundation
+
+struct User: Encodable {
+    var username: String
+    var password: String
+}
