23603510b2fa0bd669a5cae21d698bc2f23bccc7
Chris Dobek <dobek.christoher@gmail.com>
2020-04-08 16:07:15 -0400

    Storyboard Additions Complete

---
+++ Gigs/Gigs/View Controllers/GigDetailViewController.swift
@@ -0,0 +1,39 @@
+//
+//  GigDetailViewController.swift
+//  Gigs
+//
+//  Created by Chris Dobek on 4/8/20.
+//  Copyright © 2020 Chris Dobek. All rights reserved.
+//
+
+import UIKit
+
+class GigDetailViewController: UIViewController {
+    
+    
+    @IBOutlet weak var jobTitleTextField: UITextField!
+    @IBOutlet weak var dueDatePicker: UIDatePicker!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func saveButtonTapped(_ sender: Any) {
+    }
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    
+
+}
