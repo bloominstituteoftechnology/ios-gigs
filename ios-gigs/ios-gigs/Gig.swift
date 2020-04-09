7ae855fa9c5e8aee3682963dae5aa999c860f118
Shawn <shawn.james@me.com>
2020-04-09 09:49:40 -0400

    add part 2: gig model

---
+++ ios-gigs/ios-gigs/Gig.swift
@@ -0,0 +1,15 @@
+//
+//  Gig.swift
+//  ios-gigs
+//
+//  Created by Shawn James on 4/9/20.
+//  Copyright © 2020 Shawn James. All rights reserved.
+//
+
+import Foundation
+
+struct Gig: Codable {
+    let title: String
+    let dueDate: Date
+    let description: String
+}
