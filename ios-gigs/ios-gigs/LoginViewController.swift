8bf6accab17a9216c0cd690f6c19289802845c8c
Shawn <shawn.james@me.com>
2020-04-09 10:50:05 -0400

    fix signUp

--- ios-gigs/ios-gigs/LoginViewController.swift
+++ ios-gigs/ios-gigs/LoginViewController.swift
@@ -36,6 +36,7 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        loginType = .signUp
     }
     
     // MARK: - Actions
