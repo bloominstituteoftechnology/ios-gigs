99017865c5b18cf6220ebfe633b464cb7e5de068
Vincent Hoang <vietnhoang7@gmail.com>
2020-05-05 11:31:56 -0700

    Initial commit

---
+++ Gigs/Gigs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gigs
+//
+//  Created by Vincent Hoang on 5/5/20.
+//  Copyright © 2020 Vincent Hoang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
