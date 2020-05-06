cc67b7ee0ed075891d01d00de4abecf06eb0f029
Nonye <nonye@Nonyes-MacBook-Air.local>
2020-05-06 13:07:29 -0700

    Part II complete, new gig file created

---
+++ Gigs/Gigs/Gig.swift
@@ -0,0 +1,16 @@
+//
+//  Gig.swift
+//  Gigs
+//
+//  Created by Nonye on 5/6/20.
+//  Copyright © 2020 Nonye Ezekwo. All rights reserved.
+//
+
+import Foundation
+
+struct Gig: Codable {
+    var title: String
+    var dueDate: Date
+    var description: String
+
+}
