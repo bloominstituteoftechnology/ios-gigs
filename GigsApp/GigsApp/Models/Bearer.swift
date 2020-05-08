bf2f9780ae44b7f7e3052dc7b6ad80a29831c0be
Clayton Watkins <watkinclayton17@yahoo.com>
2020-05-08 09:56:24 -0700

    Completed Step 2: - Models

---
+++ GigsApp/GigsApp/Models/Bearer.swift
@@ -0,0 +1,13 @@
+//
+//  Bearer.swift
+//  GigsApp
+//
+//  Created by Clayton Watkins on 5/8/20.
+//  Copyright © 2020 Clayton Watkins. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer{
+    let token: String
+}
