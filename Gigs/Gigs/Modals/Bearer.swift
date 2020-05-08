e145cda45b7156964a97b2f04ab35f54671d97b5
ahavamorse <ahavamorse@icloud.com>
2020-05-08 11:13:07 -0700

    Models

---
+++ Gigs/Gigs/Modals/Bearer.swift
@@ -0,0 +1,13 @@
+//
+//  Bearer.swift
+//  Gigs
+//
+//  Created by Ahava on 5/8/20.
+//  Copyright © 2020 Ahava. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer: Codable {
+    let token: String
+}
