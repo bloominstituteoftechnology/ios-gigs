4b9432e9ef0027676a48edec411f63b1eb4e24d7
Clayton Watkins <watkinclayton17@yahoo.com>
2020-05-08 11:11:52 -0700

    Finished part 4

--- GigsApp/GigsApp/ViewControllers/LoginViewController.swift
+++ GigsApp/GigsApp/ViewControllers/LoginViewController.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+enum LoginType{
+    case signIn
+    case signUp
+}
+
 class LoginViewController: UIViewController {
     //MARK: - IBOutlets
     @IBOutlet weak var segmentedController: UISegmentedControl!
@@ -15,19 +20,72 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
     
+    //MARK: - Properties
+    
+    
+    var gigController: GigController?
+    var loginType = LoginType.signUp
     
     
     //MARK: - IBActions
     @IBAction func segemetedControllerChanged(_ sender: Any) {
         if segmentedController.selectedSegmentIndex == 0{
+            loginType = .signUp
             loginButton.setTitle("Sign Up", for: .normal)
         } else {
+            loginType = .signIn
             loginButton.setTitle("Sign In", for: .normal)
         }
     }
     
     @IBAction func loginButtonTapped(_ sender: Any) {
-        
+        if let username = usernameTextField.text , !username.isEmpty,
+            let password = passwordTextField.text, !password.isEmpty{
+            let user = User(username: username, password: password)
+            if loginType == .signUp {
+                gigController?.signUp(with: user, completion: { (result) in
+                    do{
+                        let success = try result.get()
+                        if success{
+                            DispatchQueue.main.async {
+                                let alertController = UIAlertController(title: "Sign Up Successful", message: "Now please log in", preferredStyle: .alert)
+                                let alertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                                alertController.addAction(alertAction)
+                                self.present(alertController, animated: true) {
+                                    self.loginType = .signIn
+                                    self.segmentedController.selectedSegmentIndex = 1
+                                    self.loginButton.setTitle("Sign In", for: .normal)
+                                }
+                            }
+                        }
+                    } catch {
+                        print("Error signing up: \(error)")
+                    }
+                })
+            } else {
+                gigController?.signIn(with: user, completion: { (result) in
+                    do{
+                        let success = try result.get()
+                        if success{
+                            DispatchQueue.main.async {
+                                self.dismiss(animated: true, completion: nil)
+                            }
+                        }
+                    } catch {
+                        if let error = error as? GigController.NetworkError{
+                            switch error {
+                            case .failedSignIn:
+                                print("Sign in failed")
+                            case .noData, .noToken:
+                                print("No data recieved")
+                            default:
+                                print("Other error occured")
+                            }
+                        }
+                    }
+                })
+            }
+        }
     }
     
 }
