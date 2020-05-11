064d3ff9130c3bf30062d56d12eb33bd9f1684f3
Bronson <bronsonmullens@icloud.com>
2020-05-11 16:31:54 -0700

    Updated project folders

---
+++ Gigs/Gigs/Models/Bearer.swift
@@ -0,0 +1,13 @@
+//
+//  Bearer.swift
+//  Gigs
+//
+//  Created by Bronson Mullens on 5/11/20.
+//  Copyright © 2020 Bronson Mullens. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer: Codable {
+    var token: String
+}
