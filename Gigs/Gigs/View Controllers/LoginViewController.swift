fbda8dbce1d96a2efb3737b0c8e222fc9eeab0f4
ahavamorse <ahavamorse@icloud.com>
2020-05-11 13:07:30 -0700

    Implemented View Controllers

--- Gigs/Gigs/View Controllers/LoginViewController.swift
+++ Gigs/Gigs/View Controllers/LoginViewController.swift
@@ -9,11 +9,19 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    enum LoginType {
+        case login
+        case signUp
+    }
 
     @IBOutlet weak var loginSegmentedControl: UISegmentedControl!
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
+    
+    var gigController: GigController?
+    var loginType: LoginType = .signUp
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,9 +30,51 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func loginSegmentControlChanged(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            loginType = .signUp
+            loginButton.setTitle("Sign Up", for: .normal)
+        } else {
+            loginType = .login
+            loginButton.setTitle("Log In", for: .normal)
+        }
     }
     
     @IBAction func loginButtonTapped(_ sender: UIButton) {
+        if loginType == .signUp,
+            let username = usernameTextField.text,
+            let password = passwordTextField.text,
+            !username.isEmpty,
+            !password.isEmpty {
+            gigController?.signUp(with: User(username: username, password: password)) { (error) in
+                if let error = error {
+                    NSLog("Error occurred during sign up: \(error)")
+                } else {
+                    DispatchQueue.main.async {
+                        let alertController = UIAlertController(title: "Sign Up Successful", message: "Now please log in.", preferredStyle: .alert)
+                        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                        alertController.addAction(alertAction)
+                        self.present(alertController, animated: true) {
+                            self.loginSegmentedControl.selectedSegmentIndex = 1
+                            self.loginType = .login
+                            self.loginButton.setTitle("Sign In", for: .normal)
+                        }
+                    }
+                }
+            }
+        } else if let username = usernameTextField.text,
+            let password = passwordTextField.text,
+            !username.isEmpty,
+            !password.isEmpty {
+            gigController?.login(with: User(username: username, password: password)) { (error) in
+                if let error = error {
+                    NSLog("Error occurred during sign up: \(error)")
+                } else {
+                    DispatchQueue.main.async {
+                        self.dismiss(animated: true, completion: nil)
+                    }
+                }
+            }
+        }
     }
     
     /*
