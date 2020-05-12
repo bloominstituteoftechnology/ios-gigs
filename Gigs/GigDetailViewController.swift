fcaec2b5913b5f58e62a25a74196022f00a7650b
Kenneth Jones <kennethjones1991@outlook.com>
2020-05-12 11:34:19 -0600

    Completed Gigs Part 2: Part 1 - Storyboard Additions

---
+++ Gigs/GigDetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  GigDetailViewController.swift
+//  Gigs
+//
+//  Created by Kenneth Jones on 5/12/20.
+//  Copyright © 2020 Kenneth Jones. All rights reserved.
+//
+
+import UIKit
+
+class GigDetailViewController: UIViewController {
+    
+    @IBOutlet weak var gigTextField: UITextField!
+    @IBOutlet weak var gigDatePicker: UIDatePicker!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func saveTapped(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
