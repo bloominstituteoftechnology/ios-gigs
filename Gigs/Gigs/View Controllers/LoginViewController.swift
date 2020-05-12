4f6849602f9d20fc88215798619135ae808bb5a4
Bronson <bronsonmullens@icloud.com>
2020-05-11 17:37:29 -0700

    Completed step 4

--- Gigs/Gigs/View Controllers/LoginViewController.swift
+++ Gigs/Gigs/View Controllers/LoginViewController.swift
@@ -18,27 +18,92 @@ class LoginViewController: UIViewController {
     
     // MARK: - IBActions
     @IBAction func signSegmentedTapped(_ sender: Any) {
+        switch signUpSignInControl.selectedSegmentIndex {
+        case 0:
+            loginType = .signUp
+            signUpSignInButton.setTitle("Sign Up", for: .normal)
+        case 1:
+            loginType = .signIn
+            signUpSignInButton.setTitle("Sign In", for: .normal)
+        default:
+            loginType = .signUp
+            signUpSignInButton.setTitle("Sign Up", for: .normal)
+        }
     }
     
     @IBAction func signButtonTapped(_ sender: Any) {
+        if let username = usernameTextField.text, !username.isEmpty,
+            let password = passwordTextField.text, !password.isEmpty {
+            let user = User(username: username, password: password)
+            if loginType == .signUp {
+                gigController.signUp(with: user, completion: { (result) in
+                    do {
+                        let success = try result.get()
+                        if success {
+                            DispatchQueue.main.async {
+                                let alertController = UIAlertController(title: "Sign Up Successful", message: "Please log in", preferredStyle: .alert)
+                                let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                                alertController.addAction(alertAction)
+                                self.present(alertController, animated: true) {
+                                    self.loginType = .signIn
+                                    self.signUpSignInControl.selectedSegmentIndex = 1
+                                    self.signUpSignInButton.setTitle("Sign In", for: .normal)
+                                }
+                            }
+                        }
+                    } catch {
+                        NSLog("Error signing up: \(error)")
+                    }
+                })
+            } else {
+                gigController.signIn(with: user, completion: { (result) in
+                    do {
+                        let success = try result.get()
+                        if success {
+                            DispatchQueue.main.async {
+                                self.dismiss(animated: true, completion: nil)
+                            }
+                        }
+                    } catch {
+                        if let error = error as? GigController.NetworkError {
+                            switch error {
+                            case .failedSignIn:
+                                NSLog("Sign in failed")
+                            case .noToken, .noData:
+                                NSLog("No data received")
+                            default:
+                                NSLog("Unknown error occured")
+                            }
+                        }
+                    }
+                })
+            }
+        }
     }
     
     // MARK: - Properties
+    var gigController = GigController()
+    var loginType: LoginType = .signUp
     
-
+    enum LoginType {
+        case signUp
+        case signIn
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
