d72163ea61afeb0d45bc7255aa95d87c3bf037e7
Bronson <bronsonmullens@icloud.com>
2020-05-13 16:55:38 -0700

    Completed part 2 of Gigs-2

---
+++ Gigs/Gigs/Models/Gig.swift
@@ -0,0 +1,15 @@
+//
+//  Gig.swift
+//  Gigs
+//
+//  Created by Bronson Mullens on 5/13/20.
+//  Copyright © 2020 Bronson Mullens. All rights reserved.
+//
+
+import Foundation
+
+struct Gig: Codable {
+    var title: String
+    var description: String
+    var dueDate: Date
+}
