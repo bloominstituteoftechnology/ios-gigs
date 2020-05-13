0061808544efb0e0566e74d8b4d67d6af5241153
Clayton Watkins <watkinclayton17@yahoo.com>
2020-05-12 21:58:16 -0700

    Completed Day 2: Part 4. Bug with program not able to get token

--- GigsApp/GigsApp/ViewControllers/GigDetailViewController.swift
+++ GigsApp/GigsApp/ViewControllers/GigDetailViewController.swift
@@ -15,16 +15,40 @@ class GigDetailViewController: UIViewController {
     @IBOutlet weak var gigDueDatePicker: UIDatePicker!
     @IBOutlet weak var gigDescriptionTextField: UITextView!
     
+    //MARK: - Properties
+    var gigController: GigController?
+    var gig: Gigs?
+    
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        updateViews()
     }
     
     //MARK: - IBAction
     @IBAction func saveButtonTapped(_ sender: Any) {
+        guard let gigTitle = gigTitleTextField.text, !gigTitle.isEmpty,
+            let gigDescription = gigDescriptionTextField.text, !gigDescription.isEmpty,
+            let gigController = gigController else { return }
+        let date = gigDueDatePicker.date
+        
+        gigController.createGig(title: gigTitle, dueDate: date, description: gigDescription) { (result) in
+            DispatchQueue.main.async {
+                self.navigationController?.popViewController(animated: true)
+            }
+        }
         
     }
     
+    //MARK: - Helper Function
+    private func updateViews(){
+        if let gig = self.gig{
+            navigationItem.title = "Gigs"
+            gigTitleTextField.text = gig.title
+            gigDueDatePicker.date = gig.dueDate
+            gigDescriptionTextField.text = gig.description
+        } else{
+            navigationItem.title = "New Gig"
+        }
+    }
 }
--- GigsApp/GigsApp/ViewControllers/GigsTableViewController.swift
+++ GigsApp/GigsApp/ViewControllers/GigsTableViewController.swift
@@ -12,12 +12,17 @@ class GigsTableViewController: UITableViewController {
     
     //MARK: - Properties
     let gigController = GigController()
+    var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .short
+        return formatter
+    }
     
     
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        gigController.getAllGigs(completion: <#T##(Result<[Gigs], GigController.NetworkError>) -> Void#>)
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -26,19 +31,38 @@ class GigsTableViewController: UITableViewController {
             performSegue(withIdentifier: "LoginSegue", sender: self)
         }
         
-        // Fetch Gigs here
+        gigController.getAllGigs { (result) in
+            do{
+                let gigs = try result.get()
+                DispatchQueue.main.async {
+                    self.gigController.gigList = gigs
+                }
+            } catch {
+                if let error = error as? GigController.NetworkError {
+                    switch error {
+                    case .noToken:
+                        print("have user try to log in again")
+                    case .noData, .tryAgain:
+                        print("have user try again")
+                    default:
+                        break
+                    }
+                }
+            }
+        }
     }
-
+    
     // MARK: - Table view data source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return 0
+        return gigController.gigList.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "", for: indexPath)
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "GigCell", for: indexPath)
+        let gigs = gigController.gigList[indexPath.row]
+        cell.textLabel?.text = gigs.title
+        cell.detailTextLabel?.text = dateFormatter.string(from: gigs.dueDate)
         return cell
     }
     
@@ -47,6 +71,16 @@ class GigsTableViewController: UITableViewController {
         if segue.identifier == "LoginSegue"{
             if let loginVC = segue.destination as? LoginViewController{
                 loginVC.gigController = gigController
+            } else if segue.identifier == "AddGigSegue"{
+                if let addGigVC = segue.destination as? GigDetailViewController{
+                    addGigVC.gigController = gigController
+                }
+            } else if segue.identifier == "GigDetailSegue"{
+                if let gigDetailVC = segue.destination as? GigDetailViewController,
+                    let indexPath = tableView.indexPathForSelectedRow{
+                    gigDetailVC.gigController = gigController
+                    gigDetailVC.gig = gigController.gigList[indexPath.row]
+                }
             }
         }
     }
