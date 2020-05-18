fd35a4f88c1fed1e5ed8d3a62c843c3d56abeaef
ahavamorse <ahavamorse@icloud.com>
2020-05-18 10:30:20 -0700

    Added fetchAllGigs and addGig methods to GigController

--- Gigs/Gigs/Controllers/GigController.swift
+++ Gigs/Gigs/Controllers/GigController.swift
@@ -15,7 +15,18 @@ class GigController {
         case post = "POST"
     }
     
+    enum NetworkError: Error {
+        case badUrl
+        case noAuth
+        case badAuth
+        case otherError
+        case badData
+        case noDecode
+    }
+    
     var bearer: Bearer?
+    var gigs: [Gig] = []
+    
     let baseURL = URL(string: "https://lambdagigapi.herokuapp.com/api")!
     
     func signUp(with user: User, completion: @escaping (Error?) -> ()) {
@@ -100,5 +111,81 @@ class GigController {
         }.resume()
     }
     
+    func fetchAllGigs(completion: @escaping (Result<[Gig], NetworkError>) -> Void) {
+        guard let bearer = bearer else {
+            completion(.failure(.noAuth))
+            return
+        }
+        
+        let allGigsUrl = baseURL.appendingPathComponent("/gigs/")
+        
+        var request = URLRequest(url: allGigsUrl)
+        request.httpMethod = HTTPMethod.get.rawValue
+        
+        request.setValue("Bearer \(bearer.token)", forHTTPHeaderField: "Authorization")
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                NSLog("Error receiving animal name data: \(error)")
+                completion(.failure(.otherError))
+                return
+            }
+            
+            if let response = response as? HTTPURLResponse,
+                response.statusCode == 401 {
+                completion(.failure(.badAuth))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(.badData))
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            do {
+                self.gigs = try decoder.decode([Gig].self, from: data)
+                completion(.success(self.gigs))
+            } catch {
+                NSLog("Error decoding animal objects: \(error)")
+                completion(.failure(.noDecode))
+                return
+            }
+        }.resume()
+    }
     
+    func addGig(with user: User, add gig: Gig, completion: @escaping (Error?) -> ()) {
+        let addGigUrl = baseURL.appendingPathComponent("/gigs/")
+        
+        var request = URLRequest(url: addGigUrl)
+        
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let jsonEncoder = JSONEncoder()
+        do {
+            let jsonData = try jsonEncoder.encode(gig)
+            request.httpBody = jsonData
+        } catch {
+            NSLog("Error encoding gig object: \(error)")
+            completion(error)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { (_, response, error) in
+            if let error = error {
+                completion(error)
+                return
+            }
+            
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                completion(NSError(domain: "", code: response.statusCode, userInfo: nil))
+                return
+            }
+            
+            self.gigs.append(gig)
+            completion(nil)
+        }.resume()
+    }
 }
