3fcd363ba283fce7a29e9408323f04725678ed0f
ahavamorse <ahavamorse@icloud.com>
2020-05-18 10:41:49 -0700

    Implemented cellForRowAt and numberOfRowsInSection in GigsTableViewController

--- Gigs/Gigs/View Controllers/GigsTableViewController.swift
+++ Gigs/Gigs/View Controllers/GigsTableViewController.swift
@@ -11,9 +11,13 @@ import UIKit
 class GigsTableViewController: UITableViewController {
 
     let gigController = GigController()
+    let dateFormatter = DateFormatter()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        dateFormatter.dateStyle = .short
+        dateFormatter.timeStyle = .none
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -28,51 +32,23 @@ class GigsTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return gigController.gigs.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "gigCell", for: indexPath)
+        
+        let gig = gigController.gigs[indexPath.row]
+        let dateString = dateFormatter.string(from: gig.dueDate)
+        
+        cell.textLabel?.text = gig.title
+        cell.detailTextLabel?.text = "\(gig.dueDate)"
+//        cell.detailTextLabel?.text = dateString
 
         return cell
     }
     
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     
     // MARK: - Navigation
