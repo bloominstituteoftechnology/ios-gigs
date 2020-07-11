20c7355bc55e93173f3ca3c8669bc1c266138914
Sammy Alvarado <isammyalvarado@gmail.com>
2020-07-11 12:36:00 -0400

    Created Func for SignUp!
    Created new function for signUp which carries errors functionality to notify the developer of error. Example if data was not sent to the API or returned.

--- Gigs/Gigs/Model Controller/GigController.swift
+++ Gigs/Gigs/Model Controller/GigController.swift
@@ -15,10 +15,48 @@ class GigiController {
         case post = "POST"
     }
 
-    private let baseURL = URL(string: "https://lambdagigs.vapor.cloud/api")
-    private lazy var signUpURL = baseURL?.appendingPathComponent("/users/signup")
-    private lazy var signInURL = baseURL?.appendingPathComponent("/users/login")
+    enum NetWorkError: Error {
+        case noData
+        case failedSignUP
+    }
+
+
+    private let baseURL = URL(string: "https://lambdagigs.vapor.cloud/api")!
+    private lazy var signUpURL = baseURL.appendingPathComponent("/users/signup")
+    private lazy var signInURL = baseURL.appendingPathComponent("/users/login")
     var bearer: Bearer?
+
+    func signUP(with user: User, completion: @escaping (Result<Bool, NetWorkError>) ->Void) {
+        print("SignUpURL = \(signUpURL.absoluteString)")
+
+        var request = postRequest(for: signUpURL)
+
+        do {
+            let jsonData = try JSONEncoder().encode(user)
+            print(String(data: jsonData, encoding: .utf8)!)
+            request.httpBody = jsonData
+
+            let task = URLSession.shared.dataTask(with: request) { (_, response, error) in
+                if let error = error {
+                    print("Sign Up failed due to Error: \(error)")
+                    completion(.failure(.failedSignUP))
+                    return
+                }
+
+                guard let response = response as? HTTPURLResponse,
+                    response.statusCode == 200 else {
+                        print("SignUp was unsuccessful")
+                        completion(.failure(.failedSignUP))
+                        return
+                }
+                completion(.success(true))
+            }
+            task.resume()
+        } catch {
+
+        }
+    }
+
 
 
     private func postRequest(for url: URL) -> URLRequest {
