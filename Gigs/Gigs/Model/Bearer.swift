b5bc53dbd3942cf624fb6008213796e43fb2ca0e
Sammy Alvarado <isammyalvarado@gmail.com>
2020-07-11 01:23:12 -0400

    Created Bearer Swift File
    Added a new Model that holds the Bearer Token.

---
+++ Gigs/Gigs/Model/Bearer.swift
@@ -0,0 +1,13 @@
+//
+//  Bearer.swift
+//  Gigs
+//
+//  Created by Sammy Alvarado on 7/11/20.
+//  Copyright © 2020 Sammy Alvarado. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer: Codable {
+    let token: String
+}
