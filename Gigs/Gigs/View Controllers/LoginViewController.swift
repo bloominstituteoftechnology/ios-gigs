db40196b0e9ad7fd61d3beb87340c8b1cfacf8e6
Eoin Lavery <me@eoinlavery.com>
2020-07-13 02:36:35 +0100

    Part 1 - Storyboard Setup Complete

---
+++ Gigs/Gigs/View Controllers/LoginViewController.swift
@@ -0,0 +1,39 @@
+//
+//  LoginViewController.swift
+//  Gigs
+//
+//  Created by Eoin Lavery on 13/07/2020.
+//  Copyright © 2020 Eoin Lavery. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBAction func loginSignupSegmentedControl(_ sender: Any) {
+    }
+    
+    @IBAction func usernameTextField(_ sender: Any) {
+    }
+    
+    @IBAction func passwordTextField(_ sender: Any) {
+    }
+    
+    @IBAction func loginSignupButton(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+    */
+
+    @IBAction func loginSignupButtonPressed(_ sender: Any) {
+    }
+    
+}
