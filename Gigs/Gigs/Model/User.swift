28e580c5c1746a59c626f4c25f3e6f5045bf4a56
Eoin Lavery <me@eoinlavery.com>
2020-07-13 02:38:30 +0100

    Part 2 - Models Completed

---
+++ Gigs/Gigs/Model/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Gigs
+//
+//  Created by Eoin Lavery on 13/07/2020.
+//  Copyright © 2020 Eoin Lavery. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    var username: String
+    var password: String
+}
