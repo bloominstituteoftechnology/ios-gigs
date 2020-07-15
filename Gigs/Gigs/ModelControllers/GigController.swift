811ffc9bfa813d52408d56772e2317fc732e238c
Cora Jacobson <cora-jacobson@lambdastudents.com>
2020-07-14 19:33:08 -0700

    Day 2 - Parts 2 & 3

--- Gigs/Gigs/ModelControllers/GigController.swift
+++ Gigs/Gigs/ModelControllers/GigController.swift
@@ -20,13 +20,17 @@ class GigController {
         case failedSignUp
         case failedSignIn
         case noToken
+        case tryAgain
     }
     
     var bearer: Bearer?
     
+    var gigs: [Gig] = []
+    
     private let baseURL = URL(string: "https://lambdagigapi.herokuapp.com/api")!
     private lazy var signUpURL = baseURL.appendingPathComponent("/users/signup")
     private lazy var signInURL = baseURL.appendingPathComponent("/users/login")
+    private lazy var gigsURL = baseURL.appendingPathComponent("/gigs/")
     
     func signUp(with user: User, completion: @escaping (Result<Bool, NetworkError>) -> Void) {
         print("signUpURL = \(signUpURL.absoluteString)")
@@ -97,6 +101,73 @@ class GigController {
         } catch {
             print("Error encoding user: \(error.localizedDescription)")
             completion(.failure(.failedSignIn))
+        }
+    }
+    
+    func getGigs(completion: @escaping (Result<[Gig], NetworkError>) -> Void) {
+        guard let bearer = bearer else {
+            completion(.failure(.noToken))
+            return
+        }
+        var request = URLRequest(url: gigsURL)
+        request.httpMethod = HTTPMethod.get.rawValue
+        request.setValue("Bearer \(bearer.token)", forHTTPHeaderField: "Authorization")
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                print("Error receiving gig data: \(error)")
+                completion(.failure(.tryAgain))
+                return
+            }
+            if let response = response as? HTTPURLResponse,
+                response.statusCode == 401 {
+                completion(.failure(.noToken))
+                return
+            }
+            guard let data = data else {
+                print("No data received from getGigs")
+                completion(.failure(.noData))
+                return
+            }
+            do {
+                let gigs = try JSONDecoder().decode([Gig].self, from: data)
+                completion(.success(gigs))
+            } catch {
+                print("Error decoding gig data: \(error)")
+                completion(.failure(.tryAgain))
+            }
+        }
+        task.resume()
+    }
+    
+    func createGig(with gig: Gig, completion: @escaping (Result<Bool, NetworkError>) -> Void) {
+        guard let bearer = bearer else {
+            completion(.failure(.noToken))
+            return
+        }
+        var request = postRequest(for: gigsURL)
+        request.setValue("Bearer \(bearer.token)", forHTTPHeaderField: "Authorization")
+        do {
+            let jsonEncoder = JSONEncoder()
+            jsonEncoder.dateEncodingStrategy = .iso8601
+            let jsonData = try jsonEncoder.encode(gig)
+            request.httpBody = jsonData
+            let task = URLSession.shared.dataTask(with: request) { (_, response, error) in
+                if let error = error {
+                    print("Error creating gig: \(error)")
+                    completion(.failure(.tryAgain))
+                    return
+                }
+                if let response = response as? HTTPURLResponse,
+                    response.statusCode == 401 {
+                    completion(.failure(.noToken))
+                    return
+                }
+                completion(.success(true))
+            }
+            task.resume()
+        } catch {
+            print("Error encoding gig: \(error)")
+            completion(.failure(.tryAgain))
         }
     }
     
---
+++ Gigs/Gigs/Models/Gig.swift
@@ -0,0 +1,15 @@
+//
+//  Gig.swift
+//  Gigs
+//
+//  Created by Cora Jacobson on 7/14/20.
+//  Copyright © 2020 Cora Jacobson. All rights reserved.
+//
+
+import Foundation
+
+struct Gig: Codable {
+    let title: String
+    let dueDate: Date
+    let description: String
+}
