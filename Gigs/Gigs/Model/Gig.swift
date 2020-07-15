3e504bdc7814b031ce17cc5a3744487a3a1392d4
Sammy Alvarado <isammyalvarado@gmail.com>
2020-07-14 21:49:24 -0400

    Added Gig Model
    Added Gig model with properties that hold a title, description, and due date data.

---
+++ Gigs/Gigs/Model/Gig.swift
@@ -0,0 +1,15 @@
+//
+//  Gig.swift
+//  Gigs
+//
+//  Created by Sammy Alvarado on 7/14/20.
+//  Copyright © 2020 Sammy Alvarado. All rights reserved.
+//
+
+import Foundation
+
+struct Gig: Codable {
+    let title: String
+    let description: String
+    let dueDate: Date
+}
