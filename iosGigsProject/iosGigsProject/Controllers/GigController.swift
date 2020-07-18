92b5f532ad1a4e026760a7bedbcd9daf34eeed21
BrysonSaclausa <brysonSaclausa@Owners-MacBook-Pro.local>
2020-07-18 08:23:14 -0700

    GigsPart 1 Complete

--- iosGigsProject/iosGigsProject/Controllers/GigController.swift
+++ iosGigsProject/iosGigsProject/Controllers/GigController.swift
@@ -11,6 +11,112 @@ import Foundation
 
 class GigController {
     
+    enum HTTPMethod: String {
+        case get = "GET"
+        case post = "POST"
+    }
+    
+    enum NetworkError: Error {
+        case nodata
+        case failedSignUp
+        case failedSignIn
+        case noToken
+    }
+    
+    var bearer: Bearer?
+    
+    let baseURL = URL(string: "https://lambdagigapi.herokuapp.com/api")!
+    private lazy var signUpURL = baseURL.appendingPathComponent("/users/signup")
+    private lazy var signInURL = baseURL.appendingPathComponent("/users/login")
+    
+   
+    
+    func signUp(with user: User, completion: @escaping (Result<Bool, NetworkError>) -> Void) {
+        print("signUpURL = \(signUpURL.absoluteString)")
+        
+        var request = postRequest(for: signUpURL)
+        
+        do {
+            let jsonData = try JSONEncoder().encode(user)
+            print(String(data: jsonData, encoding: .utf8)!)
+            request.httpBody = jsonData
+            
+            let task = URLSession.shared.dataTask(with: request) { (_, response, error) in
+            if let error = error {
+                print("Sign up failed with error: \(error)")
+                completion(.failure(.failedSignUp))
+                return
+            }
+                
+            guard let response = response as? HTTPURLResponse,
+                        response.statusCode == 200 else {
+                            print("Sign up was unsuccessful")
+                            completion(.failure(.failedSignUp))
+                            return
+                    }
+                    
+                    completion(.success(true))
+                }
+                task.resume()
+        } catch {
+           print("Error encoding user: \(error)")
+            completion(.failure(.failedSignUp))
+            return
+        }
+        
+    }
+    private func postRequest(for url: URL) -> URLRequest {
+           var request = URLRequest(url: url)
+           request.httpMethod = HTTPMethod.post.rawValue
+           request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+           return request
+       }
+       
+       func signIn(with user: User, completion: @escaping (Result<Bool, NetworkError>) -> Void) {
+           var request = postRequest(for: signInURL)
+           do {
+               let jsonData = try JSONEncoder().encode(user)
+               request.httpBody = jsonData
+               let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                   if let error = error {
+                       print("Sign in failed with error: \(error)")
+                       completion(.failure(.failedSignIn))
+                       return
+                   }
+                   guard let response = response as? HTTPURLResponse,
+                       response.statusCode == 200 else {
+                           print("Sign in was unsuccessful")
+                           completion(.failure(.failedSignIn))
+                           return
+                   }
+                   guard let data = data else {
+                       print("Data was not received")
+                    completion(.failure(.nodata))
+                       return
+                   }
+                   do {
+                       self.bearer = try JSONDecoder().decode(Bearer.self, from: data)
+                       completion(.success(true))
+                   } catch {
+                       print("Error decoding bearer: \(error)")
+                       completion(.failure(.noToken))
+                   }
+               }
+               task.resume()
+           } catch {
+               print("Error encoding user: \(error.localizedDescription)")
+               completion(.failure(.failedSignIn))
+           }
+       }
+       
+   }
 
 
-}
+
+
+
+
+
+
+
+
--- iosGigsProject/iosGigsProject/Views/LoginViewController.swift
+++ iosGigsProject/iosGigsProject/Views/LoginViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    enum LoginType {
+        case signUp
+        case signIn
+    }
+    
+    var gigController: GigController?
+    var loginType = LoginType.signUp
     
     @IBOutlet weak var segmentedController: UISegmentedControl!
     @IBOutlet weak var usernameTextField: UITextField!
@@ -19,37 +26,75 @@ class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        signInLogInButton.layer.cornerRadius = 8
         // Do any additional setup after loading the view.
     }
     
-    
-    
-    
-    
-    
-    
-
-    
-    @IBAction func signInLogInSegmentedControl(_ sender: Any) {
+    @IBAction func signInLogInSegmentedControl(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            loginType = .signUp
+            signInLogInButton.setTitle("Sign Up", for: .normal)
+        } else {
+            loginType = .signIn
+            signInLogInButton.setTitle("Sign In", for: .normal)
+        }
     }
     
     
     @IBAction func signInLogInButtonTapped(_ sender: Any) {
-    }
-    
-    
-    
-    
+    if let username = usernameTextField.text,
+                        !username.isEmpty,
+                        let password = passwordTextField.text,
+                        !password.isEmpty {
+                        let user = User(username: username, password: password)
+                        switch loginType {
+                        case .signUp:
+                            gigController?.signUp(with: user, completion: { (result) in
+                                do {
+                                    let success = try result.get()
+                                    if success {
+                                        DispatchQueue.main.async {
+                                            let alertController = UIAlertController(title: "Sign up successful!", message: "Please log in.", preferredStyle: .alert)
+                                            let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                                            alertController.addAction(alertAction)
+                                            self.present(alertController, animated: true) {
+                                                self.loginType = .signIn
+                                                self.segmentedController.selectedSegmentIndex = 1
+                                                self.signInLogInButton.setTitle("Sign In", for: .normal)
+                                            }
+                                        }
+                                    }
+                                } catch {
+                                    print("Error signing up: \(error)")
+                                }
+                            })
+                        case .signIn:
+                            gigController?.signIn(with: user, completion: { (result) in
+                                do {
+                                    let success = try result.get()
+                                    if success {
+                                        DispatchQueue.main.async {
+                                            self.dismiss(animated: true, completion: nil)
 
-    /*
-    // MARK: - Navigation
+                                        }
+                                    }
+                                } catch {
+                                    if let error = error as? GigController.NetworkError {
+                                        switch error {
+                                        case .failedSignIn:
+                                            print("Sign in failed")
+                                        case .nodata, .noToken:
+                                            print("No data received")
+                                        default:
+                                            print("Other error occurred")
+                                        }
+                                    }
+                                }
+                            })
+                        }
+                    }
+                }
+            
+        }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
-}
