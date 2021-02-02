107893517d18bc7fa21f889a1005c7a8e0eced2b
James <jameswilliammcdougall@gmail.com>
2021-02-01 21:37:13 -0600

    Part 2 complete

---
+++ Gigs/Gigs/Models/Bearer.swift
@@ -0,0 +1,15 @@
+//
+//  Bearer.swift
+//  Gigs
+//
+//  Created by James McDougall on 2/1/21.
+//
+
+import Foundation
+
+struct Bearer {
+    var id: [String:Int]
+    var token: [String: String]
+    var userId: [String: Int]
+}
+
---
+++ Gigs/Gigs/Models/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  Gigs
+//
+//  Created by James McDougall on 2/1/21.
+//
+
+import Foundation
+
+struct User {
+    var username: String
+    var password: String
+}
