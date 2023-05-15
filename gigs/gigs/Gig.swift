eb1f4aedc39e384f72188518cc2c22ece0887b48
HarmonyMorse <harmonymorse@icloud.com>
2023-05-15 15:33:44 -0700

    Day 2, part 2 done

---
+++ gigs/gigs/Gig.swift
@@ -0,0 +1,14 @@
+//
+//  Gig.swift
+//  gigs
+//
+//  Created by Harm on 5/11/23.
+//
+
+import Foundation
+
+struct Gig {
+    var title: String
+    var dueDate: Date
+    var description: String
+}
