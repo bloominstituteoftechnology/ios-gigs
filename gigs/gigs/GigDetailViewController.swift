0acd5cf10c144566160859d194ff2c0430801cce
HarmonyMorse <harmonymorse@icloud.com>
2023-05-16 15:38:08 -0700

    Day 2 part 4.2 done

--- gigs/gigs/GigDetailViewController.swift
+++ gigs/gigs/GigDetailViewController.swift
@@ -13,6 +13,9 @@ class GigDetailViewController: UIViewController {
     @IBOutlet weak var datePicker: UIDatePicker!
     @IBOutlet weak var jobDescriptionTextView: UITextView!
     
+    var gigController: GigController!
+    var gig: Gig?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,7 +23,35 @@ class GigDetailViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    //In the action of the save button, grab the values from the text field/view, and the date picker. Call the GigController's method to create (POST) a gig on the API. In the completion of this method, pop the view controller (on the correct queue) back to the table view controller.
+    
     @IBAction func saveTapped(_ sender: UIBarButtonItem) {
+        guard let title = jobTitleTextField.text, let description = jobDescriptionTextView.text else { return }
+        let newGig = Gig(title: title, dueDate: datePicker.date, description: description)
+        gigController.postGig(gig: newGig) { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let success):
+                    print("Gig posted successfully: \(success)")
+                    if let navigationController = self.navigationController {
+                        navigationController.popViewController(animated: true)
+                    }
+                case .failure(let failure):
+                    print("Error posting gig: \(failure)")
+                }
+            }
+        }
+    }
+    
+    func updateViews() {
+        if let gig = gig {
+            self.title = "Gig"
+            jobTitleTextField.text = gig.title
+            datePicker.date = gig.dueDate
+            jobDescriptionTextView.text = gig.description
+        } else {
+            self.title = "New Gig"
+        }
     }
     
     /*
